b629f93115590ce67dbaa4c1cb879d405e65e38f
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:11 -0400

    improve

--- r/530d9de0-cada-43e8-ae29-70eda8e516fe/js/grid.js
+++ r/530d9de0-cada-43e8-ae29-70eda8e516fe/js/grid.js
@@ -1,3 +1,9 @@
+/**
+ * Represents a grid of tiles.
+ * @constructor
+ * @param {number} size - The size of the grid (number of cells on each side).
+ * @param {Array<Array<Tile|null>>} [previousState] - The initial state of the grid.
+ */
 function Grid(size, previousState) {
   this.size = size;
   this.cells = previousState ? this.fromState(previousState) : this.empty();
@@ -5,12 +11,12 @@ function Grid(size, previousState) {
 
 // Build a grid of the specified size
 Grid.prototype.empty = function () {
-  var cells = [];
+  const cells = [];
 
-  for (var x = 0; x < this.size; x++) {
-    var row = cells[x] = [];
+  for (let x = 0; x < this.size; x++) {
+    const row = cells[x] = [];
 
-    for (var y = 0; y < this.size; y++) {
+    for (let y = 0; y < this.size; y++) {
       row.push(null);
     }
   }
@@ -19,13 +25,13 @@ Grid.prototype.empty = function () {
 };
 
 Grid.prototype.fromState = function (state) {
-  var cells = [];
+  const cells = [];
 
-  for (var x = 0; x < this.size; x++) {
-    var row = cells[x] = [];
+  for (let x = 0; x < this.size; x++) {
+    const row = cells[x] = [];
 
-    for (var y = 0; y < this.size; y++) {
-      var tile = state[x][y];
+    for (let y = 0; y < this.size; y++) {
+      const tile = state[x][y];
       row.push(tile ? new Tile(tile.position, tile.value) : null);
     }
   }
@@ -33,17 +39,49 @@ Grid.prototype.fromState = function (state) {
   return cells;
 };
 
-// Find the first available random position
+/**
+ * Find the first available random position
+ * @returns {null|{x: number, y: number}} A random available cell, or null if no cells are available.
+ */
 Grid.prototype.randomAvailableCell = function () {
-  var cells = this.availableCells();
+  const cells = this.availableCells();
 
   if (cells.length) {
     return cells[Math.floor(Math.random() * cells.length)];
   }
+
+  return null;
+};
+
+/**
+ * Find the first available random position, but with the cells array shuffled first
+ * to avoid always selecting the same cell when there are multiple available cells
+ * @returns {null|{x: number, y: number}} A random available cell, or null if no cells are available.
+ */
+Grid.prototype.randomAvailableCellShuffled = function () {
+  const cells = this.availableCells();
+
+  if (cells.length) {
+    this.shuffle(cells);
+    return cells[0];
+  }
+
+  return null;
+};
+
+/**
+ * Shuffle an array using the Fisher-Yates algorithm
+ * @param {Array} array - The array to shuffle
+ */
+Grid.prototype.shuffle = function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 };
 
 Grid.prototype.availableCells = function () {
-  var cells = [];
+  const cells = [];
 
   this.eachCell(function (x, y, tile) {
     if (!tile) {
@@ -54,21 +92,31 @@ Grid.prototype.availableCells = function () {
   return cells;
 };
 
-// Call callback for every cell
+/**
+ * Call callback for every cell
+ * @param {function} callback - The callback function to call for each cell
+ */
 Grid.prototype.eachCell = function (callback) {
-  for (var x = 0; x < this.size; x++) {
-    for (var y = 0; y < this.size; y++) {
+  for (let x = 0; x < this.size; x++) {
+    for (let y = 0; y < this.size; y++) {
       callback(x, y, this.cells[x][y]);
     }
   }
 };
 
-// Check if there are any cells available
+/**
+ * Check if there are any cells available
+ * @returns {boolean} True if there are available cells, false otherwise
+ */
 Grid.prototype.cellsAvailable = function () {
-  return !!this.availableCells().length;
+  return this.availableCells().length > 0;
 };
 
-// Check if the specified cell is taken
+/**
+ * Check if the specified cell is taken
+ * @param {Object} cell - The cell to check
+ * @returns {boolean} True if the cell is taken, false otherwise
+ */
 Grid.prototype.cellAvailable = function (cell) {
   return !this.cellOccupied(cell);
 };
@@ -77,15 +125,23 @@ Grid.prototype.cellOccupied = function (cell) {
   return !!this.cellContent(cell);
 };
 
+/**
+ * Get the content of the specified cell
+ * @param {Object} cell - The cell to get the content of
+ * @returns {Tile|null} The tile at the specified cell, or null if the cell is empty
+ */
 Grid.prototype.cellContent = function (cell) {
   if (this.withinBounds(cell)) {
     return this.cells[cell.x][cell.y];
-  } else {
-    return null;
   }
+
+  return null;
 };
 
-// Inserts a tile at its position
+/**
+ * Insert a tile at its position
+ * @param {Tile} tile - The tile to insert
+ */
 Grid.prototype.insertTile = function (tile) {
   this.cells[tile.x][tile.y] = tile;
 };
@@ -94,24 +150,41 @@ Grid.prototype.removeTile = function (tile) {
   this.cells[tile.x][tile.y] = null;
 };
 
+/**
+ * Check if the specified position is within the bounds of the grid
+ * @param {Object} position - The position to check
+ * @returns {boolean} True if the position is within the bounds of the grid, false otherwise
+ */
 Grid.prototype.withinBounds = function (position) {
-  return position.x >= 0 && position.x < this.size &&
-         position.y >= 0 && position.y < this.size;
+  return (
+    position.x >= 0 &&
+    position.x < this.size &&
+    position.y >= 0 &&
+    position.y < this.size
+  );
+};
+
+/**
+ * Create a deep copy of the grid
+ * @returns {Grid} A deep copy of the grid
+ */
+Grid.prototype.clone = function () {
+  return new Grid(this.size, this.serialize());
 };
 
 Grid.prototype.serialize = function () {
-  var cellState = [];
+  const cellState = [];
 
-  for (var x = 0; x < this.size; x++) {
-    var row = cellState[x] = [];
+  for (let x = 0; x < this.size; x++) {
+    const row = cellState[x] = [];
 
-    for (var y = 0; y < this.size; y++) {
+    for (let y = 0; y < this.size; y++) {
       row.push(this.cells[x][y] ? this.cells[x][y].serialize() : null);
     }
   }
 
   return {
     size: this.size,
-    cells: cellState
+    cells: cellState,
   };
 };
