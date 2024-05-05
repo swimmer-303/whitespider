abc5f1ed1ef4fba4b992394bc358049852246627
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:42 -0400

    improve

--- r/11a9609c-acf0-4557-94b1-b6b6d84f2a40/scripts/supportcheck.js
+++ r/11a9609c-acf0-4557-94b1-b6b6d84f2a40/scripts/supportcheck.js
@@ -1,1 +1,88 @@
-"use strict";!function(){var e=!!document.querySelector('script[src*="kaspersky"]'),t=document.createElement("canvas"),r=[];if(!(!t.getContext("webgl")&&!t.getContext("experimental-webgl"))||r.push("WebGL"),"undefined"==typeof WebAssembly&&r.push("WebAssembly"),"noModule"in HTMLScriptElement.prototype||r.push("JavaScript Modules"),0!==r.length||e){var o=document.createElement("div");o.id="notSupportedWrap",document.body.appendChild(o);var s=document.createElement("h2");s.id="notSupportedTitle",s.textContent=e?"Kaspersky Internet Security broke this export":"Software update needed",o.appendChild(s);var n=document.createElement("p");n.className="notSupportedMessage";var a="This content is not supported because your device's software is out-of-date. ",i=navigator.userAgent;/android/i.test(i)?a+='<br><br>On Android, fix this by making sure the <a href="https://play.google.com/store/apps/details?id=com.google.android.webview">Android System Webview</a> app has updates enabled and is up-to-date.':/iphone|ipad|ipod/i.test(i)?a+="<br><br>Note: using the <strong>iOS simulator</strong> requires <strong>Xcode 12+</strong>. Otherwise try testing on a real device instead.":!/msie/i.test(i)&&!/trident/i.test(i)||/edge\//i.test(i)?e?a="It appears a script was added to this export by Kaspersky software. This prevents the exported project from working. Try disabling Kaspersky and exporting again.":a+="Try installing any available software updates. Alternatively try on a different device.":a+="<br><br>Note: <strong>Internet Explorer</strong> is not supported. Try using <a href='https://www.google.com/chrome'>Chrome</a> or <a href='https://www.mozilla.org/firefox'>Firefox</a> instead.",a+="<br><br><em>Missing features: "+r.join(", ")+"<br>User agent: "+navigator.userAgent+"</em>",n.innerHTML=a,o.appendChild(n)}else window.C3_IsSupported=!0}();
+"use strict";
+
+function notSupported(message) {
+  const notSupportedWrap = document.createElement("div");
+  notSupportedWrap.id = "notSupportedWrap";
+  document.body.appendChild(notSupportedWrap);
+
+  const notSupportedTitle = document.createElement("h2");
+  notSupportedTitle.id = "notSupportedTitle";
+  notSupportedTitle.textContent = message.title;
+  notSupportedWrap.appendChild(notSupportedTitle);
+
+  const notSupportedMessage = document.createElement("p");
+  notSupportedMessage.className = "notSupportedMessage";
+  notSupportedMessage.innerHTML = message.content + "<br><br><em>" + message.details + "</em>";
+  notSupportedWrap.appendChild(notSupportedMessage);
+}
+
+function getMessage(reasons, isKaspersky) {
+  const userAgent = navigator.userAgent;
+
+  if (isKaspersky) {
+    return {
+      title: "Kaspersky Internet Security broke this export",
+      content: "It appears a script was added to this export by Kaspersky software. This prevents the exported project from working. Try disabling Kaspersky and exporting again.",
+      details: `Missing features: ${reasons.join(", ")}; User agent: ${userAgent}`
+    };
+  }
+
+  if (/android/i.test(userAgent)) {
+    return {
+      title: "Software update needed",
+      content: "This content is not supported because your device's software is out-of-date. On Android, fix this by making sure the <a href='https://play.google.com/store/apps/details?id=com.google.android.webview'>Android System Webview</a> app has updates enabled and is up-to-date.",
+      details: `Missing features: ${reasons.join(", ")}; User agent: ${userAgent}`
+    };
+  }
+
+  if (/iphone|ipad|ipod/i.test(userAgent)) {
+    return {
+      title: "Software update needed",
+      content: "This content is not supported because your device's software is out-of-date. Note: using the <strong>iOS simulator</strong> requires <strong>Xcode 12+</strong>. Otherwise try testing on a real device instead.",
+      details: `Missing features: ${reasons.join(", ")}; User agent: ${userAgent}`
+    };
+  }
+
+  if (!/msie/i.test(userAgent) && !/trident/i.test(userAgent) || /edge\//i.test(userAgent)) {
+    return {
+      title: "Software update needed",
+      content: isKaspersky ? "Try installing any available software updates. Alternatively try on a different device." : "This content is not supported because your device's software is out-of-date. Try installing any available software updates. Alternatively try on a different device.",
+      details: `Missing features: ${reasons.join(", ")}; User agent: ${userAgent}`
+    };
+  }
+
+  return {
+    title: "Software not supported",
+    content: isKaspersky ? "This content is not supported because your device's software is out-of-date. Try installing any available software updates. Alternatively try on a different device." : "This content is not supported. Note: <strong>Internet Explorer</strong> is not supported. Try using <a href='https://www.google.com/chrome'>Chrome</a> or <a href='https://www.mozilla.org/firefox'>Firefox</a> instead.",
+    details: `Missing features: ${reasons.join(", ")}; User agent: ${userAgent}`
+  };
+}
+
+function checkSupport() {
+  const reasons = [];
+  const canvas = document.createElement("canvas");
+
+  if (!canvas.getContext("webgl") && !canvas.getContext("experimental-webgl")) {
+    reasons.push("WebGL");
+  }
+
+  if (typeof WebAssembly === "undefined") {
+    reasons.push("WebAssembly");
+  }
+
+  if ("noModule" in HTMLScriptElement.prototype) {
+    reasons.push("JavaScript Modules");
+  }
+
+  const isKaspersky = !!document.querySelector('script[src*="kaspersky"]');
+
+  if (reasons.length > 0 || isKaspersky) {
+    const message = getMessage(reasons, isKaspersky);
+    notSupported(message);
+  } else {
+    window.C3_IsSupported = true;
+  }
+}
+
+checkSupport();
+
