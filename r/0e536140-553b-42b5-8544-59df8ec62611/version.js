83fab7f6c40221ac9895d397d1671717640ce022
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:25 -0400

    improve

--- r/0e536140-553b-42b5-8544-59df8ec62611/version.js
+++ r/0e536140-553b-42b5-8544-59df8ec62611/version.js
@@ -1,2 +1,18 @@
-version = "6";
-libs = ["./cdn.jsdelivr.net/npm/@orange-games/phaser-ads@2.2/build/phaser-ads.min.js", "./cdn.jsdelivr.net/npm/@orange-games/phaser-cachebuster@2.0/build/phaser-cachebuster.min.js", "./cdn.jsdelivr.net/npm/@orange-games/phaser-input@2.0/build/phaser-input.min.js", "./cdn.jsdelivr.net/npm/@orange-games/phaser-nineslice@2.0/build/phaser-nineslice.min.js", "./cdn.jsdelivr.net/npm/@orange-games/phaser-spine@3.0/build/phaser-spine.min.js", "./cdn.jsdelivr.net/npm/@orange-games/phaser-super-storage@1.0/build/phaser-super-storage.min.js", "./cdn.jsdelivr.net/npm/@orange-games/splash@3.5/build/splash.min.js"];
+const version = "6";
+const libs = [
+  { name: "phaser-ads", version: "2.2" },
+  { name: "phaser-cachebuster", version: "2.0" },
+  { name: "phaser-input", version: "2.0" },
+  { name: "phaser-nineslice", version: "2.0" },
+  { name: "phaser-spine", version: "3.0" },
+  { name: "phaser-super-storage", version: "1.0" },
+  { name: "splash", version: "3.5" },
+];
+
+const baseUrl = "https://cdn.jsdelivr.net/npm/@orange-games/";
+
+const libUrls = libs.map(lib => `${baseUrl}${lib.name}@${lib.version}/build/${lib.name}.min.js`);
+
+console.log("version:", version);
+console.log("libraries:", libUrls);
+
