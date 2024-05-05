0369960414bc79d06e8ed320fe17eee361f3f0c4
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:04 -0400

    improve

--- lib/jsdos/types/src/components/region.d.ts
+++ lib/jsdos/types/src/components/region.d.ts
@@ -1,6 +1,9 @@
-import { Props } from "../player-app";
-interface RegionProps extends Props {
-    class?: string;
+import { Props as PlayerAppProps } from "../player-app";
+
+export interface RegionProps extends PlayerAppProps {
+  className?: string; // changed 'class' to 'className' to follow Preact/React convention
 }
-export declare function Region(props: RegionProps): import("preact").VNode<any> | import("preact").VNode<any>[];
-export {};
+
+export declare function Region(props: RegionProps): JSX.Element | JSX.Element[];
+
+// export {} is not necessary here, so I've removed it
