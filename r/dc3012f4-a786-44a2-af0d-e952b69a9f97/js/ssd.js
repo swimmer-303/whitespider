fd2db94b571c08dfaf4d2aac66484ed07ae5be47
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:43 -0400

    improve

--- r/dc3012f4-a786-44a2-af0d-e952b69a9f97/js/ssd.js
+++ r/dc3012f4-a786-44a2-af0d-e952b69a9f97/js/ssd.js
@@ -1,61 +1,90 @@
-arcade.minesweeper.ssd = function(container, starting_value) {
-  var div = $.createElement("div").style({"width": 39, "height": 23, "background-image": "url('image/sprite.png')", "background-repeat": "no-repeat", "background-position": "-46px -81px", "padding": 1, "margin": "auto"});
-  var table = $.createElement("table").setAttribute({"cellspacing": 0, "cellpadding": 0});
+arcade.minesweeper.SSD = function(container, starting_value) {
+  if (!$.createElement) {
+    throw new Error('createElement function not defined');
+  }
+
+  var div = $.createElement("div").style({
+    width: "39px",
+    height: "23px",
+    backgroundImage: "url('image/sprite.png')",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "-46px -81px",
+    padding: "1px",
+    margin: "auto"
+  });
+
+  var table = $.createElement("table").setAttribute({
+    cellSpacing: 0,
+    cellPadding: 0
+  });
+
   var tbody = $.createElement("tbody");
+
   var tr = $.createElement("tr");
-  this.ssd_digits = [];
-  for(var i = 0; i < 3; i++) {
-    var digit = new arcade.minesweeper.ssd_digit();
-    this.ssd_digits.push(digit);
+
+  this.ssdDigits = [];
+
+  for (var i = 0; i < 3; i++) {
+    var ssdDigit = new arcade.minesweeper.SSDDigit();
+    this.ssdDigits.push(ssdDigit);
     var td = $.createElement("td");
-    td.appendChild(digit.get_element());
+    td.appendChild(ssdDigit.getElement());
     tr.appendChild(td);
   }
+
   tbody.appendChild(tr);
   table.appendChild(tbody);
   div.appendChild(table);
   container.appendChild(div);
 
-  this.set_value(starting_value);
-}
-arcade.minesweeper.ssd.prototype.set_value = function(value) {
+  this.setValue(starting_value);
+};
+
+arcade.minesweeper.SSD.prototype.setValue = function(value) {
   value = Math.min(value, 999);
   this.value = value;
 
-  if(value < 0) {
+  if (value < 0) {
     value = Math.abs(value);
-    var value_string = value + "";
-    this.ssd_digits[0].set_state("-");
-    this.ssd_digits[1].set_state(value_string[value_string.length-2]);
-    this.ssd_digits[2].set_state(value_string[value_string.length-1]);
+    var valueString = value + "";
+    this.ssdDigits[0].set_state("-");
+    this.ssdDigits[1].set_state(valueString[valueString.length - 2]);
+    this.ssdDigits[2].set_state(valueString[valueString.length - 1]);
+  } else {
+    var valueString = value + "";
+    if (valueString.length === 1) valueString = "00" + valueString;
+    if (valueString.length === 2) valueString = "0" + valueString;
+    this.ssdDigits[0].set_state(valueString[0]);
+    this.ssdDigits[1].set_state(valueString[1]);
+    this.ssdDigits[2].set_state(valueString[2]);
   }
-  else {
-    var value_string = value + "";
-    if(value_string.length === 1) value_string = "00" + value_string;
-    if(value_string.length === 2) value_string = "0" + value_string;
-    this.ssd_digits[0].set_state(value_string[0]);
-    this.ssd_digits[1].set_state(value_string[1]);
-    this.ssd_digits[2].set_state(value_string[2]);
-  }
+};
 
-}
-arcade.minesweeper.ssd.prototype.increment = function() {
-  this.set_value(this.value + 1);
-}
-arcade.minesweeper.ssd.prototype.decrement = function() {
-  this.set_value(this.value - 1);
-}
+arcade.minesweeper.SSD.prototype.increment = function() {
+  this.setValue(this.value + 1);
+};
 
-arcade.minesweeper.ssd_digit = function() {}
-arcade.minesweeper.prototype.element;
-arcade.minesweeper.ssd_digit.prototype.get_element = function() {
-  if(!this.element) {
-    this.element = $.createElement("div").style({"width": 13, "height": 23, "background-image": "url('image/sprite.png')", "background-repeat": "no-repeat", "background-position": "0px -32px"});
+arcade.minesweeper.SSD.prototype.decrement = function() {
+  this.setValue(this.value - 1);
+};
+
+arcade.minesweeper.SSDDigit = function() {};
+
+arcade.minesweeper.SSD.prototype.getElement = function() {
+  if (!this.element) {
+    this.element = $.createElement("div").style({
+      width: "13px",
+      height: "23px",
+      backgroundImage: "url('image/sprite.png')",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "0px -32px"
+    });
   }
   return this.element;
-}
-arcade.minesweeper.ssd_digit.prototype.set_state = function(state) {
-  switch(state) {
+};
+
+arcade.minesweeper.SSDDigit.prototype.set_state = function(state) {
+  switch (state) {
     case "0":
     case "1":
     case "2":
@@ -66,10 +95,14 @@ arcade.minesweeper.ssd_digit.prototype.set_state = function(state) {
     case "7":
     case "8":
     case "9":
-      this.element.style({"background-position": "-" + (state*13) + "px -32px"})
-    break;
+      this.element.style({
+        backgroundPosition: "-" + state * 13 + "px -32px"
+      });
+      break;
     case "-":
-      this.element.style({"background-position": "-130px -32px"})
-    break;
+      this.element.style({
+        backgroundPosition: "-130px -32px"
+      });
+      break;
   }
-}
+};
