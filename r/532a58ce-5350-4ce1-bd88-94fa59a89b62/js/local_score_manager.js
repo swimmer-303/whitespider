ea6af08a0f4ca9e1fe05343d22653f60c3536cd7
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:34 -0400

    improve

--- r/532a58ce-5350-4ce1-bd88-94fa59a89b62/js/local_score_manager.js
+++ r/532a58ce-5350-4ce1-bd88-94fa59a89b62/js/local_score_manager.js
@@ -1,35 +1,40 @@
-window.fakeStorage = {
-  _data: {},
-
-  setItem: function (id, val) {
-    return this._data[id] = String(val);
-  },
-
-  getItem: function (id) {
-    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
-  },
-
-  removeItem: function (id) {
-    return delete this._data[id];
-  },
-
-  clear: function () {
-    return this._data = {};
+class Storage {
+  constructor() {
+    this._data = {};
   }
-};
 
-function LocalScoreManager() {
-  var localSupported = !!window.localStorage;
+  setItem(id, val) {
+    this._data[id] = String(val);
+  }
 
-  this.key     = "bestScore_9007199254740992";
-  this.storage = localSupported ? window.localStorage : window.fakeStorage;
+  getItem(id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  }
+
+  removeItem(id) {
+    delete this._data[id];
+  }
+
+  clear() {
+    this._data = {};
+  }
 }
 
-LocalScoreManager.prototype.get = function () {
-  return this.storage.getItem(this.key) || 0;
-};
+class LocalScoreManager {
+  constructor() {
+    this.key = "bestScore_9007199254740992";
+    this.storage = this.checkLocalStorageSupport() ? window.localStorage : new Storage();
+  }
 
-LocalScoreManager.prototype.set = function (score) {
-  this.storage.setItem(this.key, score);
-};
+  checkLocalStorageSupport() {
+    return typeof window.localStorage !== "undefined" && window.localStorage !== null;
+  }
 
+  get() {
+    return this.storage.getItem(this.key) || 0;
+  }
+
+  set(score) {
+    this.storage.setItem(this.key, score);
+  }
+}
