33718e528b170f78cd508c46a1c0add879b71d74
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:24 -0400

    improve

--- r/0e536140-553b-42b5-8544-59df8ec62611/assets/filters/BlurX.js
+++ r/0e536140-553b-42b5-8544-59df8ec62611/assets/filters/BlurX.js
@@ -1,53 +1,50 @@
 /**
-* A horizontal blur filter by Mat Groves http://matgroves.com/ @Doormat23
-*/
-Phaser.Filter.BlurX = function (game) {
-
-    Phaser.Filter.call(this, game);
+ * A horizontal and vertical blur filter by Mat Groves (http://matgroves.com/) and Hiroki Nishina.
+ */
+class BlurFilter extends Phaser.Filter {
+  constructor(game) {
+    super(game);
 
     this.uniforms.blur = { type: '1f', value: 1 / 512 };
 
     this.fragmentSrc = [
-
       "precision mediump float;",
       "varying vec2 vTextureCoord;",
       "varying vec4 vColor;",
       "uniform float blur;",
       "uniform sampler2D uSampler;",
 
-        "void main(void) {",
+      "void main(void) {",
+      "  vec4 sum = vec4(0.0);",
+      "  for (float i = -4.0; i <= 4.0; i++) {",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y - 4.0 * blur)) * 0.05;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y - 3.0 * blur)) * 0.09;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y - 2.0 * blur)) * 0.12;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y - blur)) * 0.15;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y)) * 0.16;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y + blur)) * 0.15;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y + 2.0 * blur)) * 0.12;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y + 3.0 * blur)) * 0.09;",
+      "    sum += texture2D(uSampler, vec2(vTextureCoord.x + i * blur, vTextureCoord.y + 4.0 * blur)) * 0.05;",
+      "  }",
+      "  gl_FragColor = sum;",
+      "}"
+    ].join('\n');
+  }
 
-          "vec4 sum = vec4(0.0);",
+  get blur() {
+    return this.uniforms.blur.value * (1 / 512);
+  }
 
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - 4.0*blur, vTextureCoord.y)) * 0.05;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - 3.0*blur, vTextureCoord.y)) * 0.09;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - 2.0*blur, vTextureCoord.y)) * 0.12;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - blur, vTextureCoord.y)) * 0.15;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x, vTextureCoord.y)) * 0.16;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + blur, vTextureCoord.y)) * 0.15;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + 2.0*blur, vTextureCoord.y)) * 0.12;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + 3.0*blur, vTextureCoord.y)) * 0.09;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + 4.0*blur, vTextureCoord.y)) * 0.05;",
-
-          "gl_FragColor = sum;",
-
-        "}"
-    ];
-
-};
-
-Phaser.Filter.BlurX.prototype = Object.create(Phaser.Filter.prototype);
-Phaser.Filter.BlurX.prototype.constructor = Phaser.Filter.BlurX;
-
-Object.defineProperty(Phaser.Filter.BlurX.prototype, 'blur', {
-
-    get: function() {
-        return this.uniforms.blur.value / (1/7000);
-    },
-
-    set: function(value) {
-        this.dirty = true;
-        this.uniforms.blur.value = (1/7000) * value;
+  set blur(value) {
+    if (value < 0) {
+      console.error('Blur value should be non-negative.');
+      return;
     }
+    this.uniforms.blur.value = value * 512;
+    this.dirty = true;
+  }
+}
 
-});
+Object.assign(BlurFilter.prototype, Phaser.Filter.prototype);
+BlurFilter.prototype.constructor = BlurFilter;
