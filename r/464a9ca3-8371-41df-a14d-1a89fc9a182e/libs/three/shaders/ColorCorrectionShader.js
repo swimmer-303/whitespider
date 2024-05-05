ec18f776c11c9287e4a70fae85648e52a66e3444
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:53 -0400

    improve

--- r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/ColorCorrectionShader.js
+++ r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/ColorCorrectionShader.js
@@ -1,50 +1,41 @@
 /**
- * @author alteredq / http://alteredqualia.com/
+ * Color correction shader
  *
- * Color correction
+ * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.ColorCorrectionShader = {
+const ColorCorrectionShader = {
+  name: 'ColorCorrectionShader',
 
-	uniforms: {
+  uniforms: {
+    tDiffuse: { value: null },
+    powRGB: { value: new THREE.Vector3(2, 2, 2) },
+    mulRGB: { value: new THREE.Vector3(1, 1, 1) },
+    addRGB: { value: new THREE.Vector3(0, 0, 0) },
+  },
 
-		"tDiffuse": { value: null },
-		"powRGB": { value: new THREE.Vector3( 2, 2, 2 ) },
-		"mulRGB": { value: new THREE.Vector3( 1, 1, 1 ) },
-		"addRGB": { value: new THREE.Vector3( 0, 0, 0 ) }
+  vertexShader: /* glsl */ `
+    varying vec2 vUv;
 
-	},
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
 
-	vertexShader: [
+  fragmentShader: /* glsl */ `
+    uniform sampler2D tDiffuse;
+    uniform vec3 powRGB;
+    uniform vec3 mulRGB;
+    uniform vec3 addRGB;
 
-		"varying vec2 vUv;",
+    varying vec2 vUv;
 
-		"void main() {",
-
-		"	vUv = uv;",
-
-		"	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-
-		"}"
-
-	].join( "\n" ),
-
-	fragmentShader: [
-
-		"uniform sampler2D tDiffuse;",
-		"uniform vec3 powRGB;",
-		"uniform vec3 mulRGB;",
-		"uniform vec3 addRGB;",
-
-		"varying vec2 vUv;",
-
-		"void main() {",
-
-		"	gl_FragColor = texture2D( tDiffuse, vUv );",
-		"	gl_FragColor.rgb = mulRGB * pow( ( gl_FragColor.rgb + addRGB ), powRGB );",
-
-		"}"
-
-	].join( "\n" )
-
+    void main() {
+      vec3 color = texture2D(tDiffuse, vUv).rgb;
+      color = mulRGB * pow(color + addRGB, powRGB);
+      gl_FragColor = vec4(color, 1.0);
+    }
+  `,
 };
+
