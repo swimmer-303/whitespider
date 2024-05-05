06a8939ea6d245da168e9020c26f7f628f8030eb
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:46 -0400

    improve

--- r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/MirrorShader.js
+++ r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/MirrorShader.js
@@ -11,48 +11,50 @@ THREE.MirrorShader = {
 
 	uniforms: {
 
-		"tDiffuse": { value: null },
-		"side": { value: 1 }
+		tDiffuse: { value: null },
+		side: { type: 'i', value: 1 }
 
 	},
 
-	vertexShader: [
+	vertexShader: `
 
-		"varying vec2 vUv;",
+	varying vec2 vUv;
 
-		"void main() {",
+	void main() {
 
-		"	vUv = uv;",
-		"	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+		vUv = uv;
+		gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
-		"}"
+	}
 
-	].join( "\n" ),
+	`,
 
-	fragmentShader: [
+	fragmentShader: `
 
-		"uniform sampler2D tDiffuse;",
-		"uniform int side;",
+	uniform sampler2D tDiffuse;
+	uniform int side;
 
-		"varying vec2 vUv;",
+	varying vec2 vUv;
 
-		"void main() {",
+	void main() {
 
-		"	vec2 p = vUv;",
-		"	if (side == 0){",
-		"		if (p.x > 0.5) p.x = 1.0 - p.x;",
-		"	}else if (side == 1){",
-		"		if (p.x < 0.5) p.x = 1.0 - p.x;",
-		"	}else if (side == 2){",
-		"		if (p.y < 0.5) p.y = 1.0 - p.y;",
-		"	}else if (side == 3){",
-		"		if (p.y > 0.5) p.y = 1.0 - p.y;",
-		"	} ",
-		"	vec4 color = texture2D(tDiffuse, p);",
-		"	gl_FragColor = color;",
+		vec2 p = vUv;
 
-		"}"
+		if (side == 0) {
+			p.x = (p.x > 0.5) ? (1.0 - p.x) : p.x;
+		} else if (side == 1) {
+			p.x = (p.x < 0.5) ? (1.0 - p.x) : p.x;
+		} else if (side == 2) {
+			p.y = (p.y < 0.5) ? (1.0 - p.y) : p.y;
+		} else if (side == 3) {
+			p.y = (p.y > 0.5) ? (1.0 - p.y) : p.y;
+		}
 
-	].join( "\n" )
+		vec4 color = texture2D(tDiffuse, p);
+		gl_FragColor = color;
+
+	}
+
+	`
 
 };
