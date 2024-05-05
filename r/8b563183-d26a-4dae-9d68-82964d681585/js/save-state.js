3700f2cabf599d3340a6c1d841f4fb3049769b41
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:02 -0400

    improve

--- r/8b563183-d26a-4dae-9d68-82964d681585/js/save-state.js
+++ r/8b563183-d26a-4dae-9d68-82964d681585/js/save-state.js
@@ -1,63 +1,67 @@
 function exportSaveState() {
-	var state = {};
+  const state = {};
 
-	if(gameState == 1 || gameState == -1 || (gameState === 0 && localStorage.getItem('saveState') !== undefined)) {
-		state = {
-			hex: $.extend(true, {}, MainHex),
-			blocks: $.extend(true, [], blocks),
-			score: score,
-			wavegen: waveone,
-			gdx: gdx,
-			gdy: gdy,
-			comboTime:settings.comboTime
-		};
+  if (
+    gameState === 1 ||
+    gameState === -1 ||
+    (gameState === 0 && localStorage.getItem("saveState") !== null)
+  ) {
+    state = {
+      hex: {
+        ...MainHex,
+        blocks: MainHex.blocks.map((a) => {
+          return a.map((b) => {
+            const descaledBlock = descaleBlock(b);
+            return { ...descaledBlock };
+          });
+        }),
+      },
+      blocks: blocks.map((b) => {
+        const descaledBlock = descaleBlock(b);
+        return { ...descaledBlock };
+      }),
+      score,
+      wavegen: waveone,
+      gdx,
+      gdy,
+      comboTime: settings.comboTime,
+    };
+  }
 
-		state.hex.blocks.map(function(a){
-			for (var i = 0; i < a.length; i++) {
-				a[i] = $.extend(true, {}, a[i]);
-			}
+  localStorage.setItem("highscores", JSON.stringify(highscores));
 
-			a.map(descaleBlock);
-		});
-
-		for (var i = 0; i < state.blocks.length; i++) {
-			state.blocks[i] = $.extend(true, {}, state.blocks[i]);
-		}
-
-		state.blocks.map(descaleBlock);
-	}
-
-	localStorage.setItem('highscores', JSON.stringify(highscores));
-
-	return JSONfn.stringify(state);
+  return JSON.stringify(state);
 }
 
 function descaleBlock(b) {
-	b.distFromHex /= settings.scale;
+  b.distFromHex /= settings.scale;
+  return b;
 }
 
 function writeHighScores() {
-		highscores.sort(
-		function(a,b){
-			a = parseInt(a, 10);
-			b = parseInt(b, 10);
-			if (a < b) {
-				return 1;
-			} else if (a > b) {
-				return -1;
-			}else {
-				return 0;
-			}
-		}
-	);
-	highscores = highscores.slice(0,3);
-	localStorage.setItem("highscores", JSON.stringify(highscores));
+  highscores.sort((a, b) => {
+    a = parseInt(a, 10);
+    b = parseInt(b, 10);
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  highscores = highscores.slice(0, 3);
+  localStorage.setItem("highscores", JSON.stringify(highscores));
 }
 
 function clearSaveState() {
-	localStorage.setItem("saveState", "{}");
+  localStorage.setItem("saveState", "{}");
 }
 
 function isStateSaved() {
-	return localStorage.getItem("saveState") != "{}" && localStorage.getItem("saveState") != undefined;
+  return (
+    localStorage.getItem("saveState") !== "{}" &&
+    localStorage.getItem("saveState") !== null
+  );
 }
