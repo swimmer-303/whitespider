7fb64b4a77aaa1ec3cdfd8b691f889374dfb62c6
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:55:12 -0400

    improve

--- r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/postprocessing/FilmPass.js
+++ r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/postprocessing/FilmPass.js
@@ -1,51 +1,80 @@
 /**
+ * FilmPass - a postprocessing pass that applies a film grain and scanlines effect
  * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.FilmPass = function ( noiseIntensity, scanlinesIntensity, scanlinesCount, grayscale ) {
+const THREE = require('three');
 
-	var shader = THREE.ShaderExtras[ "film" ];
+const filmShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    time: { value: 0.0 },
+    nIntensity: { value: 1.0 },
+    sIntensity: { value: 1.0 },
+    sCount: { value: 100 },
+    grayscale: { value: false },
+  },
+  vertexShader: `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    }
+  `,
+  fragmentShader: `
+    uniform sampler2D tDiffuse;
+    uniform float time;
+    uniform float nIntensity;
+    uniform float sIntensity;
+    uniform float sCount;
+    uniform bool grayscale;
 
-	this.uniforms = THREE.UniformsUtils.clone( shader.uniforms );
-
-	this.material = new THREE.ShaderMaterial( {
-
-		uniforms: this.uniforms,
-		vertexShader: shader.vertexShader,
-		fragmentShader: shader.fragmentShader
-
-	} );
-
-	if ( grayscale !== undefined )	this.uniforms.grayscale.value = grayscale;
-	if ( noiseIntensity !== undefined ) this.uniforms.nIntensity.value = noiseIntensity;
-	if ( scanlinesIntensity !== undefined ) this.uniforms.sIntensity.value = scanlinesIntensity;
-	if ( scanlinesCount !== undefined ) this.uniforms.sCount.value = scanlinesCount;
-
-	this.enabled = true;
-	this.renderToScreen = false;
-	this.needsSwap = true;
-
+    void main() {
+      vec4 color = texture2D( tDiffuse, vUv );
+      float noise = nIntensity * ( 0.5 + 0.5 * sin( time + 10.0 * vUv.x + 20.0 * vUv.y ) );
+      float scanlines = sIntensity * mod( vUv.y * sCount, 1.0 );
+      if ( grayscale ) {
+        color.rgb = vec3( dot( color.rgb, vec3( 0.393, 0.769, 0.189 ) ) );
+      }
+      gl_FragColor = vec4( color.rgb + noise + scanlines, color.a );
+    }
+  `,
 };
 
-THREE.FilmPass.prototype = {
+class FilmPass {
+  constructor(noiseIntensity = 1.0, scanlinesIntensity = 1.0, scanlinesCount = 100, grayscale = false) {
+    if (THREE.ShaderExtras && THREE.ShaderExtras['film']) {
+      console.warn('THREE.FilmPass has been modified. Using custom implementation instead.');
+    }
 
-	render: function ( renderer, writeBuffer, readBuffer, delta ) {
+    this.uniforms = THREE.UniformsUtils.clone(filmShader.uniforms);
 
-		this.uniforms[ "tDiffuse" ].texture = readBuffer;
-		this.uniforms[ "time" ].value += delta;
+    this.material = new THREE.ShaderMaterial({
+      uniforms: this.uniforms,
+      vertexShader: filmShader.vertexShader,
+      fragmentShader: filmShader.fragmentShader,
+    });
 
-		THREE.EffectComposer.quad.material = this.material;
+    this.setNoiseIntensity(noiseIntensity);
+    this.setScanlinesIntensity(scanlinesIntensity);
+    this.setScanlinesCount(scanlinesCount);
+    this.setGrayscale(grayscale);
 
-		if ( this.renderToScreen ) {
+    this.enabled = true;
+    this.renderToScreen = false;
+    this.needsSwap = true;
+  }
 
-			renderer.render( THREE.EffectComposer.scene, THREE.EffectComposer.camera );
+  setNoiseIntensity(value) {
+    if (typeof value !== 'number') {
+      console.error('FilmPass: setNoiseIntensity: value must be a number.');
+      return;
+    }
+    this.uniforms.nIntensity.value = value;
+  }
 
-		} else {
-
-			renderer.render( THREE.EffectComposer.scene, THREE.EffectComposer.camera, writeBuffer, false );
-
-		}
-
-	}
-
-};
+  setScanlinesIntensity(value) {
+    if (typeof value !== 'number') {
+      console.error('FilmPass: setScanlinesIntensity: value must be a number.');
+      return;
+   
