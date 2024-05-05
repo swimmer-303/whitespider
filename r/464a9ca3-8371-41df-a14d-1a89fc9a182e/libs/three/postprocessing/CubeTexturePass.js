a4f11d7db886b6debfc11b3453c10e71ca67978c
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:00 -0400

    improve

--- r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/postprocessing/CubeTexturePass.js
+++ r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/postprocessing/CubeTexturePass.js
@@ -2,68 +2,69 @@
  * @author bhouston / http://clara.io/
  */
 
-THREE.CubeTexturePass = function ( camera, envMap, opacity ) {
+THREE.CubeTexturePass = function (camera, envMap, opacity) {
+  THREE.Pass.call(this);
 
-	THREE.Pass.call( this );
+  this.camera = camera;
 
-	this.camera = camera;
+  this.needsSwap = false;
 
-	this.needsSwap = false;
+  this.cubeShader = THREE.ShaderLib['cube'];
+  this.cubeMesh = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(10, 10, 10),
+    new THREE.ShaderMaterial({
+      uniforms: this.cubeShader.uniforms,
+      vertexShader: this.cubeShader.vertexShader,
+      fragmentShader: this.cubeShader.fragmentShader,
+      depthTest: false,
+      depthWrite: false,
+      side: THREE.BackSide,
+    })
+  );
 
-	this.cubeShader = THREE.ShaderLib[ 'cube' ];
-	this.cubeMesh = new THREE.Mesh(
-		new THREE.BoxBufferGeometry( 10, 10, 10 ),
-		new THREE.ShaderMaterial( {
-			uniforms: this.cubeShader.uniforms,
-			vertexShader: this.cubeShader.vertexShader,
-			fragmentShader: this.cubeShader.fragmentShader,
-			depthTest: false,
-			depthWrite: false,
-			side: THREE.BackSide
-		} )
-	);
+  Object.defineProperty(this.cubeMesh.material, 'envMap', {
+    get: function () {
+      return this.uniforms.envMap.value;
+    },
+  });
 
-	Object.defineProperty( this.cubeMesh.material, 'envMap', {
+  this.envMap = envMap;
+  this.opacity = opacity !== undefined ? opacity : 1.0;
 
-		get: function () {
-
-			return this.uniforms.envMap.value;
-
-		}
-
-	} );
-
-	this.envMap = envMap;
-	this.opacity = ( opacity !== undefined ) ? opacity : 1.0;
-
-	this.cubeScene = new THREE.Scene();
-	this.cubeCamera = new THREE.PerspectiveCamera();
-	this.cubeScene.add( this.cubeMesh );
-
+  this.cubeScene = new THREE.Scene();
+  const {projectionMatrix, matrixWorld} = camera;
+  this.cubeCamera = new THREE.PerspectiveCamera().copy(projectionMatrix, camera.aspect);
+  this.cubeCamera.quaternion.setFromRotationMatrix(matrixWorld);
+  this.cubeScene.add(this.cubeMesh);
 };
 
-THREE.CubeTexturePass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
+THREE.CubeTexturePass.prototype = Object.assign(Object.create(THREE.Pass.prototype), {
+  constructor: THREE.CubeTexturePass,
 
-	constructor: THREE.CubeTexturePass,
+  render: function (renderer, writeBuffer, readBuffer, deltaTime, maskActive) {
+    const oldAutoClear = renderer.autoClear;
+    renderer.autoClear = false;
 
-	render: function ( renderer, writeBuffer, readBuffer/*, deltaTime, maskActive*/ ) {
+    this.cubeCamera.projectionMatrix.copy(this.camera.projectionMatrix);
+    this.cubeCamera.quaternion.setFromRotationMatrix(this.camera.matrixWorld);
 
-		var oldAutoClear = renderer.autoClear;
-		renderer.autoClear = false;
+    this.cubeMesh.material.uniforms.envMap.value = this.envMap;
+    this.cubeMesh.material.uniforms.opacity.value = this.opacity;
+    this.cubeMesh.material.transparent = this.opacity < 1.0;
 
-		this.cubeCamera.projectionMatrix.copy( this.camera.projectionMatrix );
-		this.cubeCamera.quaternion.setFromRotationMatrix( this.camera.matrixWorld );
+    renderer.setRenderTarget(this.renderToScreen ? null : readBuffer);
+    if (this.clear) renderer.setClearColor(0x000000, 0); // setClearColor instead of clear
+    renderer.render(this.cubeScene, this.cubeCamera);
 
-		this.cubeMesh.material.uniforms.envMap.value = this.envMap;
-		this.cubeMesh.material.uniforms.opacity.value = this.opacity;
-		this.cubeMesh.material.transparent = ( this.opacity < 1.0 );
+    renderer.autoClear = oldAutoClear;
 
-		renderer.setRenderTarget( this.renderToScreen ? null : readBuffer );
-		if ( this.clear ) renderer.clear();
-		renderer.render( this.cubeScene, this.cubeCamera );
-
-		renderer.autoClear = oldAutoClear;
-
-	}
-
-} );
+    // Dispose resources when the object is garbage collected
+    this.cubeMesh.material.uniforms.envMap.value = null;
+    this.cubeMesh.material.uniforms.opacity.value = null;
+    this.cubeMesh.material.transparent = null;
+    this.envMap = null;
+    this.cubeMesh = null;
+    this.cubeScene = null;
+    this.cubeCamera = null;
+  },
+});
