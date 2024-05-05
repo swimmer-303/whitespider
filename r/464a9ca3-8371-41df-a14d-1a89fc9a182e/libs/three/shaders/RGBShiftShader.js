4233d47685820c6bad1dca63037b0e75b7ce381b
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:01 -0400

    improve

--- r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/RGBShiftShader.js
+++ r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/RGBShiftShader.js
@@ -1,13 +1,12 @@
 /**
- * @author felixturner / http://airtight.cc/
- *
  * RGB Shift Shader
  * Shifts red and blue channels from center in opposite directions
+ * along the specified axis.
  * Ported from http://kriss.cx/tom/2009/05/rgb-shift/
  * by Tom Butterworth / http://kriss.cx/tom/
  *
- * amount: shift distance (1 is width of input)
- * angle: shift angle in radians
+ * @author felixturner / http://airtight.cc/
+ * @modified mikethedj4 / https://github.com/mikethedj4
  */
 
 THREE.RGBShiftShader = {
@@ -15,8 +14,9 @@ THREE.RGBShiftShader = {
 	uniforms: {
 
 		"tDiffuse": { value: null },
-		"amount": { value: 0.005 },
-		"angle": { value: 0.0 }
+		"amount": { value: 0.01 }, // default shift amount
+		"angle": { value: 0.0 },
+		"axis": { value: 1.0 } // 1 for x-axis, 0 for y-axis
 
 	},
 
@@ -38,19 +38,33 @@ THREE.RGBShiftShader = {
 		"uniform sampler2D tDiffuse;",
 		"uniform float amount;",
 		"uniform float angle;",
+		"uniform float axis;",
 
 		"varying vec2 vUv;",
 
 		"void main() {",
 
-		"	vec2 offset = amount * vec2( cos(angle), sin(angle));",
-		"	vec4 cr = texture2D(tDiffuse, vUv + offset);",
-		"	vec4 cga = texture2D(tDiffuse, vUv);",
-		"	vec4 cb = texture2D(tDiffuse, vUv - offset);",
-		"	gl_FragColor = vec4(cr.r, cga.g, cb.b, cga.a);",
+		"	vec2 uvOffset = amount * vec2( cos(angle), axis * sin(angle));",
+		"	vec4 colorRight = texture2D(tDiffuse, vUv + uvOffset);",
+		"	vec4 colorCenter = texture2D(tDiffuse, vUv);",
+		"	vec4 colorLeft = texture2D(tDiffuse, vUv - uvOffset);",
+		"	gl_FragColor = vec4(colorRight.r, colorCenter.g, colorLeft.b, colorCenter.a);",
+
+		"}",
+
+		"void checkShaderError( WebGLProgram program, int shaderType ) {",
+
+		"	WebGLShader shader = gl.getAttachedShaders( program )[shaderType];",
+		"	if (gl.getShaderParameter(shader, gl.COMPILE_STATUS) == false) {",
+		"		console.log('Shader error: ' + gl.getShaderInfoLog(shader));",
+		"	}",
 
 		"}"
 
 	].join( "\n" )
 
 };
+
+// Check for shader compilation errors
+checkShaderError( THREE.RGBShiftShader.program, 0 ); // vertex shader
+checkShaderError( THREE.RGBShiftShader.program, 1 ); // fragment shader
