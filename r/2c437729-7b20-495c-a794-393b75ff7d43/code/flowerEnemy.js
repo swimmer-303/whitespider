aca9ab10c6a4351f43b9539b0499893607475053
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:44 -0400

    improve

--- r/2c437729-7b20-495c-a794-393b75ff7d43/code/flowerEnemy.js
+++ r/2c437729-7b20-495c-a794-393b75ff7d43/code/flowerEnemy.js
@@ -1,76 +1,107 @@
-/**
-	Represents a flower enemy.
-	Code by Rob Kleffner, 2011
-*/
+// Represents a flower enemy.
+// Code by Rob Kleffner, 2011
 
+// Constants
+const ENEMY_IMAGE = Enjine.Resources.Images["enemies"];
+const SPRITE_WIDTH = 2;
+const SPRITE_HEIGHT = 12;
+const WINGED_ENEMY_Y_PIC_O = 24;
+const GRAVITY = 0.1;
+const MAX_JUMP_TIME = 40;
+const JUMP_VELOCITY = -8;
+const SPARKLE_COUNT = 8;
+const SPARKLE_LIFETIME = 5;
+const DEAD_TIME_MIN = 1;
+
+// FlowerEnemy class
 Mario.FlowerEnemy = function(world, x, y) {
-    this.Image = Enjine.Resources.Images["enemies"];
-    this.World = world;
-    this.X = x;
-    this.Y = y;
-    this.Facing = 1;
-    this.Type = Mario.Enemy.Spiky;
-    this.Winged = false;
-    this.NoFireballDeath = false;
-    this.XPic = 0;
-    this.YPic = 6;
-    this.YPicO = 24;
-    this.Height = 12;
-    this.Width = 2;
-    this.YStart = y;
-    this.Ya = -8;
-    this.Y -= 1;
-    this.Layer = 0;
-    this.JumpTime = 0;
-    this.Tick = 0;
-    
-    var i = 0;
-    for (i = 0; i < 4; i++) {
-        this.Move();
-    }
+  if (!world || !x || !y) {
+    throw new Error("world, x, and y are required arguments");
+  }
+
+  this.world = world;
+  this.x = x;
+  this.y = y;
+  this.facing = 1;
+  this.type = Mario.Enemy.Spiky;
+  this.winged = false;
+  this.noFireballDeath = false;
+  this.xPic = 0;
+  this.yPic = 6;
+  this.yPicO = this.winged ? WINGED_ENEMY_Y_PIC_O : 6;
+  this.height = SPRITE_HEIGHT;
+  this.width = SPRITE_WIDTH;
+  this.yStart = y;
+  this.ya = 0;
+  this.layer = 0;
+  this.jumpTime = 0;
+  this.tick = 0;
+  this.deadTime = 0;
 };
 
+// Prototype methods
 Mario.FlowerEnemy.prototype = new Mario.Enemy();
 
-Mario.FlowerEnemy.prototype.Move = function() {
-    var i = 0, xd = 0;
-    if (this.DeadTime > 0) {
-        this.DeadTime--;
-        
-        if (this.DeadTime === 0) {
-            this.DeadTime = 1;
-            for (i = 0; i < 8; i++) {
-                this.World.AddSprite(new Mario.Sparkle(((this.X + Math.random() * 16 - 8) | 0)  + 4, ((this.Y + Math.random() * 8) | 0) + 4, Math.random() * 2 - 1, Math.random() * -1, 0, 1, 5));
-            }
-            this.World.RemoveSprite(this);
-        }
-        
-        this.X += this.Xa;
-        this.Y += this.Ya;
-        this.Ya *= 0.95;
-        this.Ya += 1;
-        
-        return;
+// Move method
+Mario.FlowerEnemy.prototype.move = function() {
+  if (this.deadTime > 0) {
+    this.deadTime--;
+
+    if (this.deadTime === 0) {
+      this.deadTime = DEAD_TIME_MIN;
+      for (let i = 0; i < SPARKLE_COUNT; i++) {
+        const x = (Math.random() * 16 - 8) | 0 + 4;
+        const y = (Math.random() * 8) | 0 + 4;
+        const vx = Math.random() * 2 - 1;
+        const vy = Math.random() * -1;
+        const lifetime = SPARKLE_LIFETIME;
+        this.world.addSprite(new Mario.Sparkle(x, y, vx, vy, 0, 1, lifetime));
+      }
+      this.world.removeSprite(this);
+      return;
     }
-    
-    this.Tick++;
-    
-    if (this.Y >= this.YStart) {
-        this.YStart = this.Y;
-        xd = Math.abs(Mario.MarioCharacter.X - this.X) | 0;
-        this.JumpTime++;
-        if (this.JumpTime > 40 && xd > 24) {
-            this.Ya = -8;
-        } else {
-            this.Ya = 0;
-        }
+  } else {
+    this.tick++;
+
+    if (this.y >= this.yStart) {
+      this.yStart = this.y;
+      const xd = Math.abs(Mario.MarioCharacter.X - this.x) | 0;
+      this.jumpTime++;
+
+      if (this.jumpTime > MAX_JUMP_TIME && xd > 24) {
+        this.ya = JUMP_VELOCITY;
+      } else {
+        this.ya = 0;
+      }
     } else {
-        this.JumpTime = 0;
+      this.jumpTime = 0;
     }
-    
-    this.Y += this.Ya;
-    this.Ya *= 0.9;
-    this.Ya += 0.1;
-    
-    this.XPic = (((this.Tick / 2) | 0) & 1) * 2 + (((this.Tick / 6) | 0) & 1);
+
+    this.ya += GRAVITY;
+    this.y += this.ya;
+  }
+
+  this.xPic = (((this.tick / 2) | 0) & 1) * 2 + (((this.tick / 6) | 0) & 1);
+};
+
+// Die method
+Mario.FlowerEnemy.prototype.die = function() {
+  this.deadTime = DEAD_TIME_MIN;
+};
+
+// Draw method
+Mario.FlowerEnemy.prototype.draw = function(ctx) {
+  const x = this.x - this.width / 2;
+  const y = this.y - this.height;
+  const image = ENEMY_IMAGE;
+  const sx = this.xPic * this.width;
+  const sy = this.yPicO;
+  const sw = this.width;
+  const sh = this.height;
+  const dx = x;
+  const dy = y;
+  const dw = sw;
+  const dh = sh;
+
+  ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
 };
