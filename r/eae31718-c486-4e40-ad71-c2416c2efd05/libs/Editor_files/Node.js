70928cd33363f5aa12ba538c46434c676802fa69
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:54:43 -0400

    improve

--- r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Editor_files/Node.js
+++ r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Editor_files/Node.js
@@ -1,3 +1,12 @@
+/**
+ * PGLI Diagram Node class
+ * @class
+ * @extends gamecore.Base
+ * @param {string} key - The key of the node
+ * @param {object} module - The module of the node
+ * @param {number} x - The x coordinate of the node
+ * @param {number} y - The y coordinate of the node
+ */
 var pgli = pgli || {};
 pgli.diagram = pgli.diagram || {};
 
@@ -21,102 +30,27 @@ pgli.diagram.Node = gamecore.Base.extend('Node',
 	name: null,
 	layers: null,
 	slot: null,
-	
+
 	sockets: [],
-	
+
 	width: 150,
 	height: 200,
 
+	/**
+	 * Initialize the node
+	 * @param {string} key - The key of the node
+	 * @param {object} module - The module of the node
+	 * @param {number} x - The x coordinate of the node
+	 * @param {number} y - The y coordinate of the node
+	 */
 	init: function(key, module, x, y)
 	{
 		var static = pgli.diagram.Node;
 
 		this.key = key;
-		this.module = module;
-
-		this.shape = new Kinetic.Group({
-			x: x == undefined ? 0 : x,
-			y: y == undefined ? 0 : y,
-			draggable: true
-		});
-
-		var layerCount = (module.layers != undefined ? module.layers.length : 0) + 1;
-
-		this.height = static.headerHeight + layerCount * static.layersHeight;
-
-		this.background = new Kinetic.Rect({
-			x: 0,
-			y: 0,
-			width: this.width,
-			height: this.height,
-			fill: "#222",
-			stroke: "#000",
-			strokeWidth: 0.5,
-			shadow: {
-				color: "black",
-				blur: 6,
-				offset: [0, 0],
-				opacity: 0.5
-			}
-		});
-
-		this.layers = new Kinetic.Shape({
-			drawFunc: function(ctx){
-				ctx.beginPath();
-				for(var i=0, len = this.attrs.count; i < len; ++i)
-					ctx.arc(10, 10+i*static.layersHeight, static.slotRadius, 0, Math.PI*2, true); 
-				ctx.closePath();
-				this.fill(ctx);
-			},
-			x: this.width-static.layersWidth,
-			y: static.layersMargin,
-			count: layerCount,
-			fill: "#111"
-		});
-
-		this.slot = new Kinetic.Circle({
-			x: static.slotX,
-			y: static.slotY,
-			radius: static.slotRadius,
-			fill: "#111"
-		});
-
-		this.name = new Kinetic.Text({
-			x: 20,
-			y: 6,
-			text: module.name,
-			fontSize: 13,
-			fontFamily: "Ubuntu Mono",
-			textFill: "#aaa"
-		});
-
-		this.shape.on('mousedown', function(){
-			this.moveToTop();
-		});
-
-		this.shape.add(this.background);
-		this.shape.add(this.name);
-		this.shape.add(this.layers);
-		this.shape.add(this.slot);
-	},
-
-	updateLayers: function()
-	{
-		var layerCount = (module.layers != undefined ? module.layers.length : 0) + 1;
-		this.layers.attrs.count = layerCount;
-	},
-
-	getSlot: function()
-	{
-		var static = pgli.diagram.Node;
-		return [this.shape.getX()+static.slotX, 
-				this.shape.getY()+static.slotY];
-	},
-
-	getLayerSlot: function(index)
-	{
-		var static = pgli.diagram.Node;
-		return [this.shape.getX()+this.width-static.layersWidth/2, 
-				this.shape.getY()+10+index*static.layersHeight+static.layersMargin];
-	}
-});
+		if (module) {
+			this.module = module;
+		} else {
+			console.error('Module is undefined or null');
+			return;
+	
