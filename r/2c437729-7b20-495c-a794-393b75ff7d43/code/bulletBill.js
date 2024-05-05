83a492154fc8a8ad429fa9999c2f818a3037acdb
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:41 -0400

    improve

--- r/2c437729-7b20-495c-a794-393b75ff7d43/code/bulletBill.js
+++ r/2c437729-7b20-495c-a794-393b75ff7d43/code/bulletBill.js
@@ -1,112 +1,141 @@
 /**
-	Represents a bullet bill enemy.
-	Code by Rob Kleffner, 2011
-*/
+ * Represents a bullet bill enemy.
+ * Code by Rob Kleffner, 2011
+ */
 
-Mario.BulletBill = function(world, x, y, dir) {
-	this.Image = Enjine.Resources.Images["enemies"];
-	this.World = world;
-	this.X = x;
-	this.Y = y;
-	this.Facing = dir;
-	
-	this.XPicO = 8;
-	this.YPicO = 31;
-	this.Height = 12;
-	this.Width = 4;
-	this.PicWidth = 16;
-	this.YPic = 5;
-	this.XPic = 0;
-	this.Ya = -5;
-	this.DeadTime = 0;
-	this.Dead = false;
-	this.Anim = 0;
-};
+const WIDTH = 4;
+const HEIGHT = 12;
+const SIDEWAYS_SPEED = 4;
+const DEAD_TIME_MAX = 100;
+const SPARKLE_COUNT = 8;
+const SPARKLE_SPEED_X = [1, -1];
+const SPARKLE_SPEED_Y = [-1, -1];
+const SPARKLE_LIFETIME = 5;
 
-Mario.BulletBill.prototype = new Mario.NotchSprite();
+class BulletBill extends Mario.NotchSprite {
+  constructor(world, x, y, dir) {
+    super();
+    this.Image = Enjine.Resources.Images["enemies"];
+    this.World = world;
+    this.X = x;
+    this.Y = y;
+    this.Facing = dir;
+    this.DeadTime = 0;
+    this.Dead = false;
+    this.Anim = 0;
+  }
 
-Mario.BulletBill.prototype.CollideCheck = function() {
+  /**
+   * Check for collisions with Mario.
+   */
+  CollideCheck() {
     if (this.Dead) {
-        return;
+      return;
     }
-    
-    var xMarioD = Mario.MarioCharacter.X - this.X, yMarioD = Mario.MarioCharacter.Y - this.Y;
-    if (xMarioD > -16 && xMarioD < 16) {
-        if (yMarioD > -this.Height && yMarioD < this.World.Mario.Height) {
-            if (Mario.MarioCharacter.Y > 0 && yMarioD <= 0 && (!Mario.MarioCharacter.OnGround || !Mario.MarioCharacter.WasOnGround)) {
-                Mario.MarioCharacter.Stomp(this);
-                this.Dead = true;
-                
-                this.Xa = 0;
-                this.Ya = 1;
-                this.DeadTime = 100;
-            } else {
-                Mario.MarioCharacter.GetHurt();
-            }
-        }
-    }
-};
 
-Mario.BulletBill.prototype.Move = function() {
-    var i = 0, sideWaysSpeed = 4;
-    if (this.DeadTime > 0) {
-        this.DeadTime--;
-        
-        if (this.DeadTime === 0) {
-            this.DeadTime = 1;
-            for (i = 0; i < 8; i++) {
-                this.World.AddSprite(new Mario.Sparkle(((this.X + Math.random() * 16 - 8) | 0) + 4, ((this.Y + Math.random() * 8) | 0) + 4, Math.random() * 2 - 1, Math.random() * -1, 0, 1, 5));
-            }
-            this.World.RemoveSprite(this);
+    const xMarioD = Mario.MarioCharacter.X - this.X;
+    const yMarioD = Mario.MarioCharacter.Y - this.Y;
+    if (xMarioD > -WIDTH && xMarioD < WIDTH) {
+      if (yMarioD > -HEIGHT && yMarioD < Mario.MarioCharacter.Height) {
+        if (
+          Mario.MarioCharacter.Y > 0 &&
+          yMarioD <= 0 &&
+          (!Mario.MarioCharacter.OnGround || !Mario.MarioCharacter.WasOnGround)
+        ) {
+          Mario.MarioCharacter.Stomp(this);
+          this.Dead = true;
+
+          this.Xa = 0;
+          this.Ya = 1;
+          this.DeadTime = DEAD_TIME_MAX;
+        } else {
+          Mario.MarioCharacter.GetHurt();
         }
-        
-        this.X += this.Xa;
-        this.Y += this.Ya;
-        this.Ya *= 0.95;
-        this.Ya += 1;
-        
-        return;
+      }
     }
-    
-    this.Xa = this.Facing * sideWaysSpeed;
+  }
+
+  /**
+   * Move the bullet bill.
+   */
+  Move() {
+    if (this.DeadTime > 0) {
+      this.DeadTime--;
+
+      if (this.DeadTime === 0) {
+        this.DeadTime = 1;
+        for (let i = 0; i < SPARKLE_COUNT; i++) {
+          const x = (this.X + Math.random() * 16 - 8) | 0;
+          const y = (this.Y + Math.random() * 8) | 0;
+          const speedX = Math.random() * 2 - 1;
+          const speedY = Math.random() * -1;
+          this.World.AddSprite(
+            new Mario.Sparkle(x + 4, y + 4, speedX, speedY, 0, 1, SPARKLE_LIFETIME)
+          );
+        }
+        this.World.RemoveSprite(this);
+        return;
+      }
+
+      this.X += this.Xa;
+      this.Y += this.Ya;
+      this.Ya *= 0.95;
+      this.Ya += 1;
+
+      return;
+    }
+
+    this.Xa = this.Facing * SIDEWAYS_SPEED;
     this.XFlip = this.Facing === -1;
     this.Move(this.Xa, 0);
-};
+  }
 
-Mario.BulletBill.prototype.SubMove = function(xa, ya) {
-	this.X += xa;
-	return true;
-};
+  /**
+   * Move the bullet bill by a given amount.
+   */
+  SubMove(xa, ya) {
+    this.X += xa;
+    return true;
+  }
 
-Mario.BulletBill.prototype.FireballCollideCheck = function(fireball) {
+  /**
+   * Check for collisions with a fireball.
+   */
+  FireballCollideCheck(fireball) {
     if (this.DeadTime !== 0) {
-        return false;
+      return false;
     }
-    
-    var xD = fireball.X - this.X, yD = fireball.Y - this.Y;
-    if (xD > -16 && xD < 16) {
-        if (yD > -this.Height && yD < fireball.Height) {
-            return true;
-        }
+
+    const xD = fireball.X - this.X;
+    const yD = fireball.Y - this.Y;
+    if (xD > -WIDTH && xD < WIDTH) {
+      if (yD > -HEIGHT && yD < fireball.Height) {
+        return true;
+      }
     }
     return false;
-};
+  }
 
-Mario.BulletBill.prototype.ShellCollideCheck = function(shell) {
+  /**
+   * Check for collisions with a shell.
+   */
+  ShellCollideCheck(shell) {
     if (this.DeadTime !== 0) {
-        return false;
+      return false;
     }
-    
-    var xD = shell.X - this.X, yD = shell.Y - this.Y;
-    if (xD > -16 && xD < 16) {
-        if (yD > -this.Height && yD < shell.Height) {
-            Enjine.Resources.PlaySound("kick");
-            this.Dead = true;
-            this.Xa = 0;
-            this.Ya = 1;
-            this.DeadTime = 100;
-            return true;
-        }
+
+    const xD = shell.X - this.X;
+    const yD = shell.Y - this.Y;
+    if (xD > -WIDTH && xD < WIDTH) {
+      if (yD > -HEIGHT && yD < shell.Height) {
+        Enjine.Resources.PlaySound("kick");
+        this.Dead = true;
+        this.Xa = 0;
+        this.Ya = 1;
+        this.DeadTime = DEAD_TIME_MAX;
+        return true;
+      }
     }
     return false;
-};
+  }
+}
