20308ee56235701ebff6fcc2d6b4e2e5c4f87b61
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:54:37 -0400

    improve

--- r/dc3012f4-a786-44a2-af0d-e952b69a9f97/js/face.js
+++ r/dc3012f4-a786-44a2-af0d-e952b69a9f97/js/face.js
@@ -1,59 +1,63 @@
 arcade.minesweeper.face = function(minesweeper, container) {
   this.minesweeper = minesweeper;
-  container.appendChild(this.get_element());
+  this.element = this.get_element();
+  container.appendChild(this.element);
+  return this;
 }
 
-arcade.minesweeper.face.prototype.minesweeper;
 arcade.minesweeper.face.prototype.state;
-arcade.minesweeper.face.prototype.element;
 arcade.minesweeper.face.prototype.mouse_left = 1;
 arcade.minesweeper.face.prototype.mouse_right = 3;
 
 arcade.minesweeper.face.prototype.get_element = function() {
   var self = this;
-  if(!this.element) {
-    this.element = $.createElement("div")
-      .style({"width": 26, "height": 26, "margin": "auto", "background-image": "url('image/sprite.png')", "background-repeat": "no-repeat"})
-      .addEventListener("mousedown", function() {
-        if(self.state === "smile") self.set_state("depressed_smile");
-      })
-      .addEventListener("mouseup", function() {
-        if(self.state === "depressed_smile") {
-          self.set_state("smile");
-          self.minesweeper.restart();
-        }
-      })
-      .addEventListener("mouseout", function() {
-        if(self.state === "depressed_smile") self.set_state("smile");
-      })
-      .addEventListener("mouseover", function(e) {
-        if(e.which === self.mouse_left && self.state === "smile") self.set_state("depressed_smile");
-      })
-    this.set_state("smile");
-  }
-  return this.element[0];
+  this.element = document.createElement("div");
+  this.element.style.width = "26px";
+  this.element.style.height = "26px";
+  this.element.style.margin = "auto";
+  this.element.style.backgroundImage = "url('image/sprite.png')";
+  this.element.style.backgroundRepeat = "no-repeat";
+  this.element.addEventListener("mousedown", function(e) {
+    e.preventDefault();
+    if (self.state === "smile") self.set_state("depressed_smile");
+  });
+  this.element.addEventListener("mouseup", function(e) {
+    if (self.state === "depressed_smile") {
+      self.set_state("smile");
+      self.minesweeper.restart();
+    }
+  });
+  this.element.addEventListener("mouseout", function() {
+    if (self.state === "depressed_smile") self.set_state("smile");
+  });
+  this.element.addEventListener("mouseover", function(e) {
+    if (e.which === self.mouse_left && self.state === "smile") self.set_state("depressed_smile");
+  });
+  this.set_state("smile");
+  return this.element;
 }
 
 arcade.minesweeper.face.prototype.set_state = function(state) {
   this.state = state;
-  switch(state) {
+  switch (state) {
     case "smile":
-      this.element.style({"background-position": "0px -55px"})
-    break;
+      this.element.style.backgroundPosition = "0px -55px";
+      break;
     case "depressed_smile":
-      this.element.style({"background-position": "-26px -55px"});
-    break;
+      this.element.style.backgroundPosition = "-26px -55px";
+      break;
     case "scared":
-      this.element.style({"background-position": "-52px -55px"});
-    break;
+      this.element.style.backgroundPosition = "-52px -55px";
+      break;
     case "dead":
-      this.element.style({"background-position": "-78px -55px"});
-    break;
+      this.element.style.backgroundPosition = "-78px -55px";
+      break;
     case "sunglasses":
-      this.element.style({"background-position": "-104px -55px"});
-    break;
+      this.element.style.backgroundPosition = "-104px -55px";
+      break;
   }
 }
+
 arcade.minesweeper.face.prototype.get_state = function() {
   return this.state;
 }
