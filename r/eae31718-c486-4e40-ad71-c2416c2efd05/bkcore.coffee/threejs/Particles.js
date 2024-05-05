37df12cf913eb7cec7f4d79a08770e2470253acc
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:54:51 -0400

    improve

--- r/eae31718-c486-4e40-ad71-c2416c2efd05/bkcore.coffee/threejs/Particles.js
+++ r/eae31718-c486-4e40-ad71-c2416c2efd05/bkcore.coffee/threejs/Particles.js
@@ -7,7 +7,6 @@
   @author Thibaut 'BKcore' Despoulain <http://bkcore.com>
 */
 
-
 (function() {
   var Particle, Particles, exports, _base;
 
@@ -25,31 +24,31 @@
       this.black = new THREE.Color(0x000000);
       this.white = new THREE.Color(0xffffff);
       this.material = new THREE.ParticleBasicMaterial({
-        color: (_ref = opts.tint) != null ? _ref : 0xffffff,
-        map: (_ref1 = opts.texture) != null ? _ref1 : null,
-        size: (_ref2 = opts.size) != null ? _ref2 : 4,
-        blending: (_ref3 = opts.blending) != null ? _ref3 : THREE.AdditiveBlending,
-        depthTest: (_ref4 = opts.depthTest) != null ? _ref4 : false,
-        transparent: (_ref5 = opts.transparent) != null ? _ref5 : true,
+        color: (_ref = opts.tint) !== null ? _ref : 0xffffff,
+        map: (_ref1 = opts.texture) !== null ? _ref1 : null,
+        size: (_ref2 = opts.size) !== null ? _ref2 : 4,
+        blending: (_ref3 = opts.blending) !== null ? _ref3 : THREE.AdditiveBlending,
+        depthTest: (_ref4 = opts.depthTest) !== null ? _ref4 : false,
+        transparent: (_ref5 = opts.transparent) !== null ? _ref5 : true,
         vertexColors: true,
-        opacity: (_ref6 = opts.opacity) != null ? _ref6 : 1.0,
+        opacity: (_ref6 = opts.opacity) !== null ? _ref6 : 1.0,
         sizeAttenuation: true
       });
-      this.max = (_ref7 = opts.max) != null ? _ref7 : 1000;
-      this.spawnRate = (_ref8 = opts.spawnRate) != null ? _ref8 : 0;
-      this.spawn = (_ref9 = opts.spawn) != null ? _ref9 : new THREE.Vector3();
-      this.velocity = (_ref10 = opts.velocity) != null ? _ref10 : new THREE.Vector3();
-      this.randomness = (_ref11 = opts.randomness) != null ? _ref11 : new THREE.Vector3();
-      this.force = (_ref12 = opts.force) != null ? _ref12 : new THREE.Vector3();
-      this.spawnRadius = (_ref13 = opts.spawnRadius) != null ? _ref13 : new THREE.Vector3();
-      this.life = (_ref14 = opts.life) != null ? _ref14 : 60;
+      this.max = (_ref7 = opts.max) !== null ? _ref7 : 1000;
+      this.spawnRate = (_ref8 = opts.spawnRate) !== null ? _ref8 : 0;
+      this.spawn = (_ref9 = opts.spawn) !== null ? _ref9 : new THREE.Vector3();
+      this.velocity = (_ref10 = opts.velocity) !== null ? _ref10 : new THREE.Vector3();
+      this.randomness = (_ref11 = opts.randomness) !== null ? _ref11 : new THREE.Vector3();
+      this.force = (_ref12 = opts.force) !== null ? _ref12 : new THREE.Vector3();
+      this.spawnRadius = (_ref13 = opts.spawnRadius) !== null ? _ref13 : new THREE.Vector3();
+      this.life = (_ref14 = opts.life) !== null ? _ref14 : 60;
       this.ageing = 1 / this.life;
-      this.friction = (_ref15 = opts.friction) != null ? _ref15 : 1.0;
-      this.color = new THREE.Color((_ref16 = opts.color) != null ? _ref16 : 0xffffff);
-      this.color2 = opts.color2 != null ? new THREE.Color(opts.color2) : null;
-      this.position = (_ref17 = opts.position) != null ? _ref17 : new THREE.Vector3();
-      this.rotation = (_ref18 = opts.rotation) != null ? _ref18 : new THREE.Vector3();
-      this.sort = (_ref19 = opts.sort) != null ? _ref19 : false;
+      this.friction = (_ref15 = opts.friction) !== null ? _ref15 : 1.0;
+      this.color = new THREE.Color((_ref16 = opts.color) !== null ? _ref16 : 0xffffff);
+      this.color2 = opts.color2 !== null ? new THREE.Color(opts.color2) : null;
+      this.position = (_ref17 = opts.position) !== null ? _ref17 : new THREE.Vector3();
+      this.rotation = (_ref18 = opts.rotation) !== null ? _ref18 : new THREE.Vector3();
+      this.sort = (_ref19 = opts.sort) !== null ? _ref19 : false;
       this.pool = [];
       this.buffer = [];
       this.geometry = null;
@@ -74,7 +73,7 @@
         p.velocity.copy(this.velocity).addSelf(this.randomVector().multiplySelf(this.randomness));
         p.force.copy(this.force);
         p.basecolor.copy(this.color);
-        if (this.color2 != null) {
+        if (this.color2 !== null) {
           p.basecolor.lerpSelf(this.color2, Math.random());
         }
         _results.push(p.life = 1.0);
@@ -93,7 +92,7 @@
       this.geometry.dynamic = true;
       this.pool = [];
       this.buffer = [];
-      for (i = _i = 0, _ref = this.max; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+      for (i = _i = 0; 0 <= _ref = this.max ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
         p = new bkcore.threejs.Particle();
         this.pool.push(p);
         this.buffer.push(p);
@@ -101,8 +100,8 @@
         this.geometry.colors.push(p.color);
       }
       this.system = new THREE.ParticleSystem(this.geometry, this.material);
-      this.system.position = this.position;
-      this.system.rotation = this.rotation;
+      this.system.position.copy(this.position);
+      this.system.rotation.copy(this.rotation);
       return this.system.sort = this.sort;
     };
 
@@ -197,7 +196,7 @@
   */
 
 
-  exports = exports != null ? exports : this;
+  exports = exports !== null ? exports : this;
 
   exports.bkcore || (exports.bkcore = {});
 
