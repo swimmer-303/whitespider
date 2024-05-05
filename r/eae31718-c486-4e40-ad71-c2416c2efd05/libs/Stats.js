9ba551cc30e7886e98d6f644074e3ba0af746063
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:54:31 -0400

    improve

--- r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Stats.js
+++ r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Stats.js
@@ -1,6 +1,127 @@
-// stats.js r10 - http://github.com/mrdoob/stats.js
-var Stats=function(){var l=Date.now(),m=l,g=0,n=1E3,o=0,h=0,p=1E3,q=0,r=0,s=0,f=document.createElement("div");f.id="stats";f.addEventListener("mousedown",function(b){b.preventDefault();t(++s%2)},!1);f.style.cssText="width:80px;opacity:0.9;cursor:pointer";var a=document.createElement("div");a.id="fps";a.style.cssText="padding:0 0 3px 3px;text-align:left;background-color:#002";f.appendChild(a);var i=document.createElement("div");i.id="fpsText";i.style.cssText="color:#0ff;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px";
-i.innerHTML="FPS";a.appendChild(i);var c=document.createElement("div");c.id="fpsGraph";c.style.cssText="position:relative;width:74px;height:30px;background-color:#0ff";for(a.appendChild(c);74>c.children.length;){var j=document.createElement("span");j.style.cssText="width:1px;height:30px;float:left;background-color:#113";c.appendChild(j)}var d=document.createElement("div");d.id="ms";d.style.cssText="padding:0 0 3px 3px;text-align:left;background-color:#020;display:none";f.appendChild(d);var k=document.createElement("div");
-k.id="msText";k.style.cssText="color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px";k.innerHTML="MS";d.appendChild(k);var e=document.createElement("div");e.id="msGraph";e.style.cssText="position:relative;width:74px;height:30px;background-color:#0f0";for(d.appendChild(e);74>e.children.length;)j=document.createElement("span"),j.style.cssText="width:1px;height:30px;float:left;background-color:#131",e.appendChild(j);var t=function(b){s=b;switch(s){case 0:a.style.display=
-"block";d.style.display="none";break;case 1:a.style.display="none",d.style.display="block"}};return{domElement:f,setMode:t,begin:function(){l=Date.now()},end:function(){var b=Date.now();g=b-l;n=Math.min(n,g);o=Math.max(o,g);k.textContent=g+" MS ("+n+"-"+o+")";var a=Math.min(30,30-30*(g/200));e.appendChild(e.firstChild).style.height=a+"px";r++;b>m+1E3&&(h=Math.round(1E3*r/(b-m)),p=Math.min(p,h),q=Math.max(q,h),i.textContent=h+" FPS ("+p+"-"+q+")",a=Math.min(30,30-30*(h/100)),c.appendChild(c.firstChild).style.height=
-a+"px",m=b,r=0);return b},update:function(){l=this.end()}}};
+// stats.js r10 - https://github.com/mrdoob/stats.js
+
+var Stats = function () {
+  var timestamp = Date.now();
+  var previousTimestamp = timestamp;
+  var frameCount = 0;
+  var fps = 0;
+  var fpsMin = 0;
+  var fpsMax = 0;
+  var ms = 0;
+  var msMin = 0;
+  var msMax = 0;
+  var frameTime = 0;
+  var fpsInterval = 1000 / 60; // 60 FPS
+  var fpsGraph = new Array(74);
+  var fpsGraphIndex = 0;
+  var msGraph = new Array(74);
+  var msGraphIndex = 0;
+  var statsEl = document.createElement("div");
+  statsEl.id = "stats";
+  statsEl.addEventListener("mousedown", function (event) {
+    event.preventDefault();
+    toggleDisplay(!statsEl.style.display);
+  }, false);
+  statsEl.style.cssText =
+    "width:80px;opacity:0.9;cursor:pointer;display:none;";
+
+  var fpsEl = document.createElement("div");
+  fpsEl.id = "fps";
+  fpsEl.style.cssText =
+    "padding:0 0 3px 3px;text-align:left;background-color:#002;";
+  statsEl.appendChild(fpsEl);
+
+  var fpsTextEl = document.createElement("div");
+  fpsTextEl.id = "fpsText";
+  fpsTextEl.style.cssText =
+    "color:#0ff;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px;";
+  fpsTextEl.innerHTML = "FPS";
+  fpsEl.appendChild(fpsTextEl);
+
+  var fpsGraphEl = document.createElement("div");
+  fpsGraphEl.id = "fpsGraph";
+  fpsGraphEl.style.cssText =
+    "position:relative;width:74px;height:30px;background-color:#0ff;";
+  for (fpsEl.appendChild(fpsGraphEl); 74 > fpsGraphEl.children.length; ) {
+    var spanEl = document.createElement("span");
+    spanEl.style.cssText =
+      "width:1px;height:30px;float:left;background-color:#113;";
+    fpsGraphEl.appendChild(spanEl);
+  }
+
+  var msEl = document.createElement("div");
+  msEl.id = "ms";
+  msEl.style.cssText =
+    "padding:0 0 3px 3px;text-align:left;background-color:#020;display:none;";
+  statsEl.appendChild(msEl);
+
+  var msTextEl = document.createElement("div");
+  msTextEl.id = "msText";
+  msTextEl.style.cssText =
+    "color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px;";
+  msTextEl.innerHTML = "MS";
+  msEl.appendChild(msTextEl);
+
+  var msGraphEl = document.createElement("div");
+  msGraphEl.id = "msGraph";
+  msGraphEl.style.cssText =
+    "position:relative;width:74px;height:30px;background-color:#0f0;";
+  for (msEl.appendChild(msGraphEl); 74 > msGraphEl.children.length; ) {
+    var spanEl = document.createElement("span");
+    spanEl.style.cssText =
+      "width:1px;height:30px;float:left;background-color:#131;";
+    msGraphEl.appendChild(spanEl);
+  }
+
+  function toggleDisplay(display) {
+    statsEl.style.display = display ? "block" : "none";
+  }
+
+  this.domElement = statsEl;
+  this.setMode = toggleDisplay;
+  this.begin = function () {
+    previousTimestamp = timestamp;
+  };
+
+  this.end = function () {
+    timestamp = Date.now();
+    frameTime = timestamp - previousTimestamp;
+    frameCount++;
+
+    if (frameTime >= fpsInterval) {
+      fps = Math.round(1000 / (frameTime / frameCount));
+      fpsMin = Math.min(fpsMin, fps);
+      fpsMax = Math.max(fpsMax, fps);
+
+      fpsTextEl.textContent = fps + " FPS (" + fpsMin + "-" + fpsMax + ")";
+      var barHeight = Math.min(30, 30 - 30 * (fps / 100));
+      fpsGraphEl.appendChild(fpsGraphEl.firstChild).style.height =
+        barHeight + "px";
+
+      ms = frameTime;
+      msMin = Math.min(msMin, ms);
+      msMax = Math.max(msMax, ms);
+
+      var barHeight = Math.min(30, 30 - 30 * (ms / 200));
+      msGraphEl.appendChild(msGraphEl.firstChild).style.height =
+        barHeight + "px";
+
+      previousTimestamp = timestamp;
+      frameCount = 0;
+    }
+
+    msTextEl.textContent = ms + " MS (" + msMin + "-" + msMax + ")";
+
+    fpsGraphIndex = (fpsGraphIndex + 1) % fpsGraph.length;
+    fpsGraph[fpsGraphIndex] = frameTime;
+
+    msGraphIndex = (msGraphIndex + 1) % msGraph.length;
+    msGraph[msGraphIndex] = frameTime;
+
+    return timestamp;
+  };
+
+  this.update = function () {
+    this.end();
+  };
+};
