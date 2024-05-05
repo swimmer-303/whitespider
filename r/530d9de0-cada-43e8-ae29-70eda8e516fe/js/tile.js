311addf5ad2fac089a954434f558d56f090e3286
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:40 -0400

    improve

--- r/530d9de0-cada-43e8-ae29-70eda8e516fe/js/tile.js
+++ r/530d9de0-cada-43e8-ae29-70eda8e516fe/js/tile.js
@@ -1,19 +1,30 @@
-function Tile(position, value) {
-  this.x                = position.x;
-  this.y                = position.y;
-  this.value            = value || 2;
-
-  this.previousPosition = null;
-  this.mergedFrom       = null; // Tracks tiles that merged together
+function Tile(position, value = 2) {
+  Object.assign(this, {
+    x: position.x,
+    y: position.y,
+    value,
+    previousPosition: null,
+    mergedFrom: null
+  });
 }
 
-Tile.prototype.savePosition = function () {
-  this.previousPosition = { x: this.x, y: this.y };
+Tile.prototype.move = function (position) {
+  this.savePosition();
+  this.updatePosition(position);
 };
 
 Tile.prototype.updatePosition = function (position) {
   this.x = position.x;
   this.y = position.y;
+};
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.set = function (value, position) {
+  this.value = value;
+  this.move(position);
 };
 
 Tile.prototype.serialize = function () {
@@ -24,4 +35,8 @@ Tile.prototype.serialize = function () {
     },
     value: this.value
   };
+};
+
+Tile.prototype.copy = function () {
+  return new Tile(this.serialize());
 };
