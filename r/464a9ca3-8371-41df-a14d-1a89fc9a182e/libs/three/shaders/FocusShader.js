6d2533b88edc073d138360ef97fcbb99323d781f
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:08 -0400

    improve

--- r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/FocusShader.js
+++ r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/FocusShader.js
@@ -1,9 +1,10 @@
 /**
- * @author alteredq / http://alteredqualia.com/
- *
  * Focus shader
  * based on PaintEffect postprocess from ro.me
  * http://code.google.com/p/3-dreams-of-black/source/browse/deploy/js/effects/PaintEffect.js
+ *
+ * @author alteredq / http://alteredqualia.com/
+ * @author mrdoob / http://mrdoob.com/
  */
 
 THREE.FocusShader = {
@@ -11,8 +12,7 @@ THREE.FocusShader = {
 	uniforms: {
 
 		"tDiffuse": { value: null },
-		"screenWidth": { value: 1024 },
-		"screenHeight": { value: 1024 },
+		"resolution": { value: new THREE.Vector2() },
 		"sampleDistance": { value: 0.94 },
 		"waveFactor": { value: 0.00125 }
 
@@ -33,59 +33,39 @@ THREE.FocusShader = {
 
 	fragmentShader: [
 
-		"uniform float screenWidth;",
-		"uniform float screenHeight;",
 		"uniform float sampleDistance;",
 		"uniform float waveFactor;",
-
 		"uniform sampler2D tDiffuse;",
+		"uniform vec2 resolution;",
 
 		"varying vec2 vUv;",
 
 		"void main() {",
 
-		"	vec4 color, org, tmp, add;",
-		"	float sample_dist, f;",
-		"	vec2 vin;",
-		"	vec2 uv = vUv;",
+		"	vec4 color = texture2D( tDiffuse, vUv );",
+		"	vec2 vin = ( vUv - vec2( 0.5 ) ) * vec2( 1.4 ) * resolution.xy;",
+		"	float sample_dist = dot( vin, vin ) * 2.0;",
+		"	vec2 sampleSize = vec2( 1.0 / resolution.x, 1.0 / resolution.y ) * vec2( 4.0 * sampleDistance * waveFactor * 100.0 + sample_dist );",
+		"	vec4 add = vec4( 0.0 );",
 
-		"	add = color = org = texture2D( tDiffuse, uv );",
+		"	add += texture2D( tDiffuse, vUv + vec2( 0.111964, 0.993712 ) * sampleSize );",
+		"	add += texture2D( tDiffuse, vUv + vec2( 0.846724, 0.532032 ) * sampleSize );",
+		"	add += texture2D( tDiffuse, vUv + vec2( 0.943883, -0.330279 ) * sampleSize );",
+		"	add += texture2D( tDiffuse, vUv + vec2( 0.330279, -0.943883 ) * sampleSize );",
+		"	add += texture2D( tDiffuse, vUv + vec2( -0.532032, -0.846724 ) * sampleSize );",
+		"	add += texture2D( tDiffuse, vUv + vec2( -0.993712, -0.111964 ) * sampleSize );",
+		"	add += texture2D( tDiffuse, vUv + vec2( -0.707107, 0.707107 ) * sampleSize );",
 
-		"	vin = ( uv - vec2( 0.5 ) ) * vec2( 1.4 );",
-		"	sample_dist = dot( vin, vin ) * 2.0;",
-
-		"	f = ( waveFactor * 100.0 + sample_dist ) * sampleDistance * 4.0;",
-
-		"	vec2 sampleSize = vec2(  1.0 / screenWidth, 1.0 / screenHeight ) * vec2( f );",
-
-		"	add += tmp = texture2D( tDiffuse, uv + vec2( 0.111964, 0.993712 ) * sampleSize );",
-		"	if( tmp.b < color.b ) color = tmp;",
-
-		"	add += tmp = texture2D( tDiffuse, uv + vec2( 0.846724, 0.532032 ) * sampleSize );",
-		"	if( tmp.b < color.b ) color = tmp;",
-
-		"	add += tmp = texture2D( tDiffuse, uv + vec2( 0.943883, -0.330279 ) * sampleSize );",
-		"	if( tmp.b < color.b ) color = tmp;",
-
-		"	add += tmp = texture2D( tDiffuse, uv + vec2( 0.330279, -0.943883 ) * sampleSize );",
-		"	if( tmp.b < color.b ) color = tmp;",
-
-		"	add += tmp = texture2D( tDiffuse, uv + vec2( -0.532032, -0.846724 ) * sampleSize );",
-		"	if( tmp.b < color.b ) color = tmp;",
-
-		"	add += tmp = texture2D( tDiffuse, uv + vec2( -0.993712, -0.111964 ) * sampleSize );",
-		"	if( tmp.b < color.b ) color = tmp;",
-
-		"	add += tmp = texture2D( tDiffuse, uv + vec2( -0.707107, 0.707107 ) * sampleSize );",
-		"	if( tmp.b < color.b ) color = tmp;",
-
-		"	color = color * vec4( 2.0 ) - ( add / vec4( 8.0 ) );",
-		"	color = color + ( add / vec4( 8.0 ) - color ) * ( vec4( 1.0 ) - vec4( sample_dist * 0.5 ) );",
+		"	color = mix( color, add, vec4( step( color.b, add.b ) ) );",
 
 		"	gl_FragColor = vec4( color.rgb * color.rgb * vec3( 0.95 ) + color.rgb, 1.0 );",
 
 		"}"
 
-
 	].join( "\n" )
 };
+
+// Set the resolution uniform
+THREE.FocusShader.uniforms.resolution.value.x = window.innerWidth;
+THREE.FocusShader.uniforms.resolution.value.y = window.innerHeight;
+
