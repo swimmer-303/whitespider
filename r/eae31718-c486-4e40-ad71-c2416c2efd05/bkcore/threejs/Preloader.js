fbd29d952a2e7e83198002d8396a118372481116
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:54:02 -0400

    improve

--- r/eae31718-c486-4e40-ad71-c2416c2efd05/bkcore/threejs/Preloader.js
+++ r/eae31718-c486-4e40-ad71-c2416c2efd05/bkcore/threejs/Preloader.js
@@ -1,12 +1,12 @@
-/*!
+/**
  * @class bkcore.threejs.Preloader
  *
  * Displays a small 3D preloader scene
- * 
+ *
  * @author Thibaut 'BKcore' Despoulain <http://bkcore.com>
  */
 
-/*!
+/**
  * @package bkcore.threejs
  */
 var bkcore = bkcore || {};
@@ -14,138 +14,170 @@ bkcore.threejs = bkcore.threejs || {};
 
 /**
  * Creates a new preloader scene.
- * You have to update Preloader.ratio with the % loaded info (float 0.0-1.0) 
+ * You have to update Preloader.ratio with the % loaded info (float 0.0-1.0)
+ *
  * @param {Object{width, height, scale, line}} opts
  */
-bkcore.threejs.Preloader = function(opts)
-{
-	this.document = opts.document || document;
+bkcore.threejs.Preloader = function (opts) {
+  this.document = opts.document || document;
 
-	this.end = false;
+  this.end = false;
 
-	this.time = 0.0;
-	this.y = 0.3;
-	this.ratio = 0.0;
+  this.time = 0.0;
+  this.y = 0.3;
+  this.ratio = 0.0;
 
-	this.height = opts.height;
-	this.width = opts.width;
+  this.height = opts.height;
+  this.width = opts.width;
 
-	this.scale = opts.scale == undefined ? 10 : opts.scale
-	this.line = opts.line == undefined ? 3 : opts.line;
+  this.scale = opts.scale == undefined ? 10 : opts.scale;
+  this.line = opts.line == undefined ? 3 : opts.line;
 
-	this.container = opts.container;
+  this.container = opts.container;
 
-	this.renderer = new THREE.CanvasRenderer({
-		clearColor: 0xffffff
-	});
-	this.renderer.setSize( opts.width, opts.height );
+  try {
+    this.renderer = new THREE.WebGLRenderer({ clearColor: 0xffffff });
+  } catch (e) {
+    console.error("WebGL not supported, falling back to CanvasRenderer.");
+    this.renderer = new THREE.CanvasRenderer({ clearColor: 0xffffff });
+  }
 
-	this.container.appendChild( this.renderer.domElement );
+  this.renderer.setSize(opts.width, opts.height);
+  this.container.appendChild(this.renderer.domElement);
 
-	this.ctx = this.renderer.domElement.getContext('2d');
-	this.ctx.textAlign = "center";
+  this.ctx = this.renderer.domElement.getContext("2d");
+  this.ctx.textAlign = "center";
 
-	this.scene = new THREE.Scene();
+  this.scene = new THREE.Scene();
 
-	this.camera = new THREE.PerspectiveCamera( 70, opts.width / opts.height, 1, 1000 );
-	this.camera.position.z = 100;
-	this.scene.add( this.camera );
+  try {
+    this.camera = new THREE.OrthographicCamera(
+      -this.width / 2,
+      this.width / 2,
+      this.height / 2,
+      -this.height / 2,
+      1,
+      1000
+    );
+  } catch (e) {
+    console.error("Orthographic camera not supported, falling back to PerspectiveCamera.");
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      opts.width / opts.height,
+      1,
+      1000
+    );
+  }
+  this.camera.position.z = 100;
+  this.scene.add(this.camera);
 
-	this.stage = new THREE.Object3D();
-	this.stage.position.set(0,10,0);
-	this.scene.add(this.stage);
+  this.stage = new THREE.Object3D();
+  this.stage.position.set(0, 10, 0);
+  this.scene.add(this.stage);
 
-	this.cube = new THREE.Mesh( new THREE.CubeGeometry( this.scale, this.scale, this.scale, 1, 1, 1 ), 
-		new THREE.MeshBasicMaterial( { color: 0x999999 } ) );
-	
-	this.cube.scale.set(0.0,0.0,0.0);
-	this.stage.add(this.cube);
+  this.cube = new THREE.Mesh(
+    new THREE.CubeGeometry(this.scale, this.scale, this.scale, 1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x999999})
+  );
 
-	this.cubew = new THREE.Mesh( new THREE.CubeGeometry( this.scale, this.scale, this.scale, 1, 1, 1 ), 
-		new THREE.MeshBasicMaterial( { 
-			wireframe: true, 
-			wireframeLinewidth: this.line, 
-			//wireframeLinecap: 'square',
-			//wireframeLinejoin: 'square',
-			color: 0xffffff
-		} ) );
-	this.cube.add(this.cubew);
+  this.cube.scale.set(0.0, 0.0, 0.0);
+  this.stage.add(this.cube);
 
-	this.outercube = new THREE.Mesh( new THREE.CubeGeometry( this.scale, this.scale, this.scale, 1, 1, 1 ), 
-		new THREE.MeshBasicMaterial( { 
-			wireframe: true, 
-			wireframeLinewidth: this.line, 
-			//wireframeLinecap: 'square',
-			//wireframeLinejoin: 'square',
-			color: 0x0093d8 
-		} ) );
-	this.stage.add(this.outercube);
+  this.cubew = new THREE.Mesh(
+    new THREE.CubeGeometry(this.scale, this.scale, this.scale, 1, 1, 1),
+    new THREE.MeshBasicMaterial({
+      wireframe: true,
+      wireframeLinewidth: this.line,
+      color: 0xffffff,
+    })
+  );
+  this.cube.add(this.cubew);
 
-	var self = this;
+  this.outercube = new THREE.Mesh(
+    new THREE.CubeGeometry(this.scale, this.scale, this.scale, 1, 1, 1),
+    new THREE.MeshBasicMaterial({
+      wireframe: true,
+      wireframeLinewidth: this.line,
+      color: 0x0093d8,
+    })
+  );
+  this.stage.add(this.outercube);
 
-	function raf()
-	{
-		if(!self.end) 
-		{
-			requestAnimationFrame( raf );
-			self.render();
-		}
-	}
-	raf();
+  const self = this;
 
-	function mm(e){
-		self.mouseMove.call(self, e);
-	}
+  function raf() {
+    if (!self.end) {
+      requestIdAnimationFrame(raf);
+      self.render();
+    }
+  }
+  raf();
 
-	this.mmsave = mm;
+  function mm(e) {
+    self.mouseMove.call(self, e);
+  }
 
-	this.document.addEventListener( 'mousemove', mm, false );
-}
+  this.mmsave = mm;
+
+  this.document.addEventListener("mousemove", mm, false);
+};
 
 /**
  * Render method to be called from a RAF loop
  */
-bkcore.threejs.Preloader.prototype.render = function()
-{
-	this.time += 0.02;
+bkcore.threejs.Preloader.prototype.render = function () {
+  this.time += 0.02;
 
-	this.ctx.clearRect(0 , 0 , this.width , this.height);
+  this.ctx.clearRect(0, 0, this.width, this.height);
 
-	var s = (this.ratio - this.cube.scale.x) * 0.3;
+  const s = (this.ratio - this.cube.scale.x) * 0.3;
 
-	this.cube.scale.addScalar(s);
-	this.cube.rotation.y = this.time;
-	this.outercube.rotation.y = this.time;
+  this.cube.scale.addScalar(s);
+  this.cube.rotation.y = this.time;
+  this.outercube.rotation.y = this.time;
 
-	this.stage.rotation.x += (this.y - this.stage.rotation.x)*0.3;
+  this.stage.rotation.x +=
+    (this.y - this.stage.rotation.x) * 0.3;
 
-	this.renderer.render( this.scene, this.camera );
+  this.renderer.render(this.scene, this.camera);
 
-	this.ctx.save();
-	this.ctx.font = "40px Arial";
-    this.ctx.fillStyle = "rgb(200, 200, 200)";
-    this.ctx.fillText(Math.round(this.ratio*100), this.width/2, this.height/2+30);
-    this.ctx.restore();
-}
+  this.ctx.save();
+  this.ctx.font = "40px Arial";
+  this.ctx.fillStyle = "rgb(200, 200, 200)";
+  this.ctx.fillText(
+    Math.round(this.ratio * 100),
+    this.width / 2,
+    this.height / 2 + 30
+  );
+  this.ctx.restore();
+};
 
-bkcore.threejs.Preloader.prototype.mouseMove = function(event) 
-{
-	var h2 = this.height/2;
-	this.y = -(event.clientY - h2)/h2+0.3;
-}
+bkcore.threejs.Preloader.prototype.mouseMove = function (event) {
+  const h2 = this.height / 2;
+  this.y = -(event.clientY - h2) / h2 + 0.3;
+};
+
+/**
+ * Sets the preloader's ratio
+ *
+ * @param {number} r - The new ratio
+ */
+bkcore.threejs.Preloader.prototype.setRatio = function (r) {
+  this.ratio = r;
+};
 
 /**
  * Deletes the Preloader
  */
-bkcore.threejs.Preloader.prototype.remove = function()
-{
-	this.document.removeEventListener( 'mousemove', this.mm, false );
-	this.end = true;
-	this.renderer = null;
-	this.scene = null;
-	this.stage = null;
-	this.cube = null;
-	this.cubew = null;
-	this.innercube = null;
-	this.container.innerHTML = "";
-}
+bkcore.threejs.Preloader.prototype.remove = function () {
+  this.document.removeEventListener("mousemove", this.mm, false);
+  this.end = true;
+  this.renderer.dispose();
+  this.scene.dispose();
+  this.camera.dispose();
+  this.stage.dispose();
+  this.cube.dispose();
+  this.cubew.dispose();
+  this.outercube.dispose();
+  this.container.innerHTML = "";
+};
