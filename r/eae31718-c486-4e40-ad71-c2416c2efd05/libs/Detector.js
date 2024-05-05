8b076e1b3e3e1a2014ef6516cf5bb84db8b35d31
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:57 -0400

    improve

--- r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Detector.js
+++ r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Detector.js
@@ -3,57 +3,53 @@
  * @author mr.doob / http://mrdoob.com/
  */
 
-var Detector = {
+const Detector = {
+  canvas: 'CanvasRenderingContext2D' in window,
+  webgl: (function() {
+    try {
+      const canvas = document.createElement('canvas');
+      return 'WebGLRenderingContext' in window && canvas.getContext('experimental-webgl');
+    } catch(e) {
+      return false;
+    }
+  })(),
+  workers: 'Worker' in window,
+  fileapi: 'File' in window && 'FileReader' in window && 'FileList' in window && 'Blob' in window,
 
-	canvas: !! window.CanvasRenderingContext2D,
-	webgl: ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )(),
-	workers: !! window.Worker,
-	fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+  getWebGLErrorMessage: function() {
+    const element = document.createElement('div');
+    element.id = 'webgl-error-message';
+    element.style.fontFamily = 'monospace';
+    element.style.fontSize = '13px';
+    element.style.fontWeight = 'normal';
+    element.style.textAlign = 'center';
+    element.style.background = '#fff';
+    element.style.color = '#000';
+    element.style.padding = '1.5em';
+    element.style.width = '400px';
+    element.style.margin = '5em auto 0';
 
-	getWebGLErrorMessage: function () {
+    if (!this.webgl) {
+      const message = this.webgl = window.WebGLRenderingContext
+        ? ['Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+          'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.']
+        .join('\n')
+        : ['Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+          'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.']
+        .join('\n');
 
-		var element = document.createElement( 'div' );
-		element.id = 'webgl-error-message';
-		element.style.fontFamily = 'monospace';
-		element.style.fontSize = '13px';
-		element.style.fontWeight = 'normal';
-		element.style.textAlign = 'center';
-		element.style.background = '#fff';
-		element.style.color = '#000';
-		element.style.padding = '1.5em';
-		element.style.width = '400px';
-		element.style.margin = '5em auto 0';
+      element.innerHTML = message.trim();
+    }
 
-		if ( ! this.webgl ) {
+    return element;
+  },
 
-			element.innerHTML = window.WebGLRenderingContext ? [
-				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
-				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-			].join( '\n' ) : [
-				'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
-				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-			].join( '\n' );
+  addGetWebGLMessage: function(parameters) {
+    const parent = parameters.parent !== undefined ? parameters.parent : document.body;
+    const id = parameters.id !== undefined ? parameters.id : 'oldie';
+    const element = this.getWebGLErrorMessage();
+    element.id = id;
 
-		}
-
-		return element;
-
-	},
-
-	addGetWebGLMessage: function ( parameters ) {
-
-		var parent, id, element;
-
-		parameters = parameters || {};
-
-		parent = parameters.parent !== undefined ? parameters.parent : document.body;
-		id = parameters.id !== undefined ? parameters.id : 'oldie';
-
-		element = Detector.getWebGLErrorMessage();
-		element.id = id;
-
-		parent.appendChild( element );
-
-	}
-
+    parent.insertBefore(element, parent.firstChild);
+  }
 };
