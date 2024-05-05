be4f7520d76caa118cbe761911285f05cfddcfad
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:27 -0400

    improve

--- r/11a9609c-acf0-4557-94b1-b6b6d84f2a40/scripts/offlineclient.js
+++ r/11a9609c-acf0-4557-94b1-b6b6d84f2a40/scripts/offlineclient.js
@@ -1,1 +1,37 @@
-"use strict";{class s{constructor(){this._broadcastChannel="undefined"==typeof BroadcastChannel?null:new BroadcastChannel("offline"),this._queuedMessages=[],this._onMessageCallback=null,this._broadcastChannel&&(this._broadcastChannel.onmessage=s=>this._OnBroadcastChannelMessage(s))}_OnBroadcastChannelMessage(s){this._onMessageCallback?this._onMessageCallback(s):this._queuedMessages.push(s)}SetMessageCallback(s){this._onMessageCallback=s;for(let s of this._queuedMessages)this._onMessageCallback(s);this._queuedMessages.length=0}}window.OfflineClientInfo=new s}
+"use strict";
+
+class OfflineClientInfo {
+  constructor() {
+    this._broadcastChannel =
+      typeof BroadcastChannel !== "undefined"
+        ? new BroadcastChannel("offline")
+        : null;
+    this._queuedMessages = [];
+    this._onMessageCallback = null;
+
+    if (this._broadcastChannel) {
+      this._broadcastChannel.onmessage = this._OnBroadcastChannelMessage.bind(this);
+    }
+  }
+
+  _OnBroadcastChannelMessage(event) {
+    if (this._onMessageCallback) {
+      this._onMessageCallback(event);
+    } else {
+      this._queuedMessages.push(event);
+    }
+  }
+
+  setMessageCallback(callback) {
+    this._onMessageCallback = callback;
+
+    for (const event of this._queuedMessages) {
+      this._onMessageCallback(event);
+    }
+
+    this._queuedMessages.length = 0;
+  }
+}
+
+window.OfflineClientInfo = new OfflineClientInfo();
+
