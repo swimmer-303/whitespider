16488838e7ceb031f18522ea0370790a5422600f
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:53:44 -0400

    improve

--- r/dacb36a8-c5c8-4728-8603-1f856396d296/ajax.js
+++ r/dacb36a8-c5c8-4728-8603-1f856396d296/ajax.js
@@ -1,6 +1,18 @@
-function ajax(url,callback){
-	var ajaxRequest;
-	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
-	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText);}}}
-	ajaxRequest.open('GET',url+'&nocache='+(new Date().getTime()),true);ajaxRequest.send(null);
+function ajax(url, callback) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        if (callback) {
+          callback(xhr.responseText);
+        }
+      } else {
+        console.error(`Error ${xhr.status}: ${xhr.statusText}`);
+      }
+    }
+  };
+
+  xhr.open('GET', url + '&nocache=' + new Date().getTime(), true);
+  xhr.send();
 }
