3cfe972dc1070522180ab06cf8ebb883eeac6e93
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:54:39 -0400

    improve

--- r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Editor_files/Timer.js
+++ r/eae31718-c486-4e40-ad71-c2416c2efd05/libs/Editor_files/Timer.js
@@ -1,166 +1,151 @@
 /*!
- * @class bkcore.Timer
+ * @package bkcore
  *
- * new Date().getTime() wrapper to use as timers.
- * 
+ * A simple timer class for measuring elapsed time.
+ *
  * @author Thibaut 'BKcore' Despoulain <http://bkcore.com>
  */
 
 /**
  * RAF shim
  */
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       || 
-          window.webkitRequestAnimationFrame || 
-          window.mozRequestAnimationFrame    || 
-          window.oRequestAnimationFrame      || 
-          window.msRequestAnimationFrame     || 
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
+const requestAnimFrame = (function() {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
 })();
 
-/*!
- * @package bkcore
- */
-var bkcore = bkcore || {};
+const bkcore = bkcore || {};
 
-/*!
- * Creates a new timer, inactive by default.
- * Call Timer.start() to activate.
- */
-bkcore.Timer = function()
-{
-	this.time = {
-		start: 0,
-		current: 0,
-		previous: 0,
-		elapsed: 0,
-		delta: 0
-	}
+class Timer {
+  /**
+   * Creates a new timer, inactive by default.
+   * Call Timer.start() to activate.
+   */
+  constructor() {
+    this.time = {
+      start: 0,
+      current: 0,
+      previous: 0,
+      elapsed: 0,
+      delta: 0,
+    };
 
-	this.active = false;
-}
+    this.active = false;
+  }
 
-/*!
- * Starts/restarts the timer.
- */
-bkcore.Timer.prototype.start = function()
-{
-	var now = new Date().getTime();
+  /**
+   * Starts/restarts the timer.
+   */
+  start() {
+    const now = new Date().getTime();
 
-	this.time.start = now;
-	this.time.current = now;
-	this.time.previous = now;
-	this.time.elapsed = 0;
-	this.time.delta = 0;
+    this.time.start = now;
+    this.time.current = now;
+    this.time.previous = now;
+    this.time.elapsed = 0;
+    this.time.delta = 0;
 
-	this.active = true;
-}
+    this.active = true;
+  }
 
-/*!
- * Restarts timer, returning last ms tick
- */
-bkcore.Timer.prototype.restart = function()
-{
-	var now = new Date().getTime();
-	var e = now - this.time.start;
+  /**
+   * Restarts timer, returning last ms tick
+   *
+   * @returns {number} The elapsed time in milliseconds.
+   */
+  restart() {
+    const now = new Date().getTime();
+    const elapsed = now - this.time.start;
 
-	this.time.start = now;
-	this.time.current = now;
-	this.time.previous = now;
-	this.time.elapsed = 0;
-	this.time.delta = 0;
+    this.start();
 
-	this.active = true;
+    return elapsed;
+  }
 
-	return e;
-}
+  /**
+   * Pauses(true)/Unpauses(false) the timer.
+   *
+   * @param {boolean} pause - Whether to pause the timer.
+   */
+  pause(pause) {
+    this.active = !pause;
+  }
 
-/*!
- * Pauses(true)/Unpauses(false) the timer.
- *
- * @param bool Do pause
- */
-bkcore.Timer.prototype.pause = function(bool)
-{
-	this.active = !bool;
-}
+  /**
+   * Update method to be called inside a RAF loop
+   *
+   * @returns {number} The elapsed time in milliseconds.
+   */
+  update() {
+    if (!this.active) return;
 
-/*!
- * Update method to be called inside a RAF loop
- */
-bkcore.Timer.prototype.update = function()
-{
-	if(!this.active) return;
+    const now = new Date().getTime();
 
-	var now = new Date().getTime();
+    this.time.current = now;
+    this.time.elapsed = this.time.current - this.time.start;
+    this.time.delta = now - this.time.previous;
+    this.time.previous = now;
 
-	this.time.current = now;
-	this.time.elapsed = this.time.current - this.time.start;
-	this.time.delta = now - this.time.previous;
-	this.time.previous = now;
+    return this.time.elapsed;
+  }
 
-	return this.time.elapsed;
-}
+  /**
+   * Returns elapsed milliseconds
+   *
+   * @returns {number} The elapsed time in milliseconds.
+   */
+  getElapsed() {
+    return this.time.elapsed;
+  }
 
-/*!
- * Returns elapsed milliseconds
- */
-bkcore.Timer.prototype.getElapsed = function()
-{
-	return this.time.elapsed;
-}
+  /**
+   * Returns a formatted version of the current elapsed time using msToTime().
+   *
+   * @param {string} [format='h:m:s:ms'] - The format string for the output.
+   *                                       Supported placeholders: h, m, s, ms.
+   *
+   * @returns {string} The formatted elapsed time.
+   */
+  getElapsedTime(format = 'h:m:s:ms') {
+    const time = this.msToTime(this.time.elapsed);
 
-/*!
- * Returns a formatted version of the current elapsed time using msToTime().
- *
- * 
- */
-bkcore.Timer.prototype.getElapsedTime = function()
-{
-	return bkcore.Timer.msToTime(this.time.elapsed);
-}
+    return format
+      .replace('h', time.h.toString().padStart(2, '0'))
+      .replace('m', time.m.toString().padStart(2, '0'))
+      .replace('s', time.s.toString().padStart(2, '0'))
+      .replace('ms', time.ms.toString().padStart(3, '0'));
+  }
 
-/*!
- * Formats a millisecond integer into a h/m/s/ms object
- * 
- * @param x int In milliseconds
- * @return Object{h,m,s,ms}
- */
-bkcore.Timer.msToTime = function(t)
-{
-	var ms, s, m, h;
-	
-	ms = t%1000;
+  /**
+   * Formats a millisecond integer into a h/m/s/ms object
+   *
+   * @param {number} x - The time in milliseconds.
+   *
+   * @returns {Object} An object with h, m, s, and ms properties.
+   */
+  msToTime(t) {
+    const ms = t % 1000;
+    const s = Math.floor((t / 1000) % 60);
+    const m = Math.floor((t / 60000) % 60);
+    const h = Math.floor(t / 3600000);
 
-	s = Math.floor((t/1000)%60);
+    return { h, m, s, ms };
+  }
 
-	m = Math.floor((t/60000)%60);
-	h = Math.floor((t/3600000));
-
-	return {h:h, m:m, s:s, ms:ms};
-}
-
-/*!
- * Formats a millisecond integer into a h/m/s/ms object with prefix zeros
- * 
- * @param x int In milliseconds
- * @return Object<string>{h,m,s,ms}
- */
-bkcore.Timer.msToTimeString = function(t)
-{
-	var ms, s, m, h;
-	
-	ms = t%1000;
-	if(ms < 10) ms = "00"+ms;
-	else if(ms < 100) ms = "0"+ms;
-
-	s = Math.floor((t/1000)%60);
-	if(s < 10) s = "0"+s;
-
-	m = Math.floor((t/60000)%60);
-	h = Math.floor((t/3600000));
-
-	return {h:h, m:m, s:s, ms:ms};
-}
+  /**
+   * Formats a millisecond integer into a h/m/s/ms object with prefix zeros
+   *
+   * @param {number} x - The time in milliseconds.
+   *
+   * @returns {Object<string>} An object with h, m, s, and ms properties.
+   */
+  msToTimeString(t) {
+    const ms = (t % 1000)
