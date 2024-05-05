18f914bea6b0215c2655693897c1e3b627bd68d7
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:31 -0400

    improve

--- r/2c437729-7b20-495c-a794-393b75ff7d43/code/music.js
+++ r/2c437729-7b20-495c-a794-393b75ff7d43/code/music.js
@@ -1,48 +1,48 @@
 /*
-* using cross platform MIDI library MIDI.js http://www.midijs.net/
+* using cross platform MIDI library MIDI.js <http://www.midijs.net/>
 */
 
-var midifiles = {
-	"title" : "midi/title.mid",
-	"map" : "midi/map.mid",
-	"background" : "midi/background.mid",
-	"overground" : "midi/overground.mid",
-	"underground" : "midi/underground.mid",
-	"castle" : "midi/castle.mid",
+const midiFiles = {
+  title: "midi/title.mid",
+  map: "midi/map.mid",
+  background: "midi/background.mid",
+  overground: "midi/overground.mid",
+  underground: "midi/underground.mid",
+  castle: "midi/castle.mid",
 };
 
-Mario.PlayMusic = function(name) {
-	if(name in midifiles)
-	{
-		// Currently we stop all playing tracks when playing a new one
-		// MIDIjs can't play multiple at one time
-		//MIDIjs.stop();;
-		//MIDIjs.play(midifiles[name]);
-	}else{
-		console.error("Cannot play music track " + name + " as i have no data for it.");
-	}
+const DEFAULT_MIDI_FILE = Object.keys(midiFiles)[0];
+
+Mario.PlayMusic = function (name = DEFAULT_MIDI_FILE) {
+  if (midiFiles.hasOwnProperty(name)) {
+    MIDIjs.stop();
+    MIDIjs.play(midiFiles[name])
+      .catch((error) => console.error(`Error playing music track ${name}:`, error));
+  } else {
+    console.error(`Cannot play music track ${name} as I have no data for it.`);
+  }
 };
 
-Mario.PlayTitleMusic = function() {
-	Mario.PlayMusic("title");
+Mario.PlayTitleMusic = function () {
+  Mario.PlayMusic("title");
 };
 
-Mario.PlayMapMusic = function() {
-	Mario.PlayMusic("map");
+Mario.PlayMapMusic = function () {
+  Mario.PlayMusic("map");
 };
 
-Mario.PlayOvergroundMusic = function() {
-	Mario.PlayMusic("background");
+Mario.PlayOvergroundMusic = function () {
+  Mario.PlayMusic("background");
 };
 
-Mario.PlayUndergroundMusic = function() {
-	Mario.PlayMusic("underground");
+Mario.PlayUndergroundMusic = function () {
+  Mario.PlayMusic("underground");
 };
 
-Mario.PlayCastleMusic = function() {
-	Mario.PlayMusic("castle");
+Mario.PlayCastleMusic = function () {
+  Mario.PlayMusic("castle");
 };
 
-Mario.StopMusic = function() {
-	//MIDIjs.stop();
+Mario.StopMusic = function () {
+  MIDIjs.stop();
 };
