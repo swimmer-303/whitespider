96e10486c3b4b90e32ae74327e25f4ce6aee5ac6
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:03 -0400

    improve

--- lib/jsdos/types/src/backend/v7/personal.d.ts
+++ lib/jsdos/types/src/backend/v7/personal.d.ts
@@ -1,2 +1,4 @@
-export declare function getPersonalBundleUrl(namespace: string, id: string, bundleUrl: string): string;
-export declare function putPersonalBundle(namespace: string, id: string, bundleUrl: string, data: Uint8Array): Promise<void>;
+import { Uint8Array } from 'buffer';
+
+export declare function getPersonalBundleDownloadUrl(namespace: string, id: string, bundleUrl: string): string;
+export declare function uploadPersonalBundle(namespace: string, id: string, bundleUrl: string, data: Uint8Array): Promise<void>;
