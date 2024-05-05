27c1cdbe67409ab1f11330ae77612cc8387a070a
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:41 -0400

    improve

--- r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/postprocessing/DotScreenPass.js
+++ r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/postprocessing/DotScreenPass.js
@@ -2,55 +2,46 @@
  * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.DotScreenPass = function ( center, angle, scale ) {
+THREE.DotScreenPass = function (center, radians, scale) {
+  THREE.Pass.call(this);
 
-	THREE.Pass.call( this );
+  if (THREE.DotScreenShader === undefined)
+    console.error('THREE.DotScreenPass relies on THREE.DotScreenShader');
 
-	if ( THREE.DotScreenShader === undefined )
-		console.error( "THREE.DotScreenPass relies on THREE.DotScreenShader" );
+  const shader = THREE.DotScreenShader;
+  this.uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-	var shader = THREE.DotScreenShader;
+  if (center !== undefined) this.uniforms["center"].value.copy(center);
+  if (radians !== undefined) this.uniforms["radians"].value = radians;
+  if (scale !== undefined) this.uniforms["scale"].value = scale;
 
-	this.uniforms = THREE.UniformsUtils.clone( shader.uniforms );
+  this.material = new THREE.ShaderMaterial({
+    uniforms: this.uniforms,
+    vertexShader: shader.vertexShader,
+    fragmentShader: shader.fragmentShader,
+  });
 
-	if ( center !== undefined ) this.uniforms[ "center" ].value.copy( center );
-	if ( angle !== undefined ) this.uniforms[ "angle" ].value = angle;
-	if ( scale !== undefined ) this.uniforms[ "scale" ].value = scale;
-
-	this.material = new THREE.ShaderMaterial( {
-
-		uniforms: this.uniforms,
-		vertexShader: shader.vertexShader,
-		fragmentShader: shader.fragmentShader
-
-	} );
-
-	this.fsQuad = new THREE.Pass.FullScreenQuad( this.material );
-
+  this.fsQuad = new THREE.Pass.FullScreenQuad(this.material);
 };
 
-THREE.DotScreenPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
+THREE.DotScreenPass.prototype = Object.assign(Object.create(THREE.Pass.prototype), {
+  constructor: THREE.DotScreenPass,
 
-	constructor: THREE.DotScreenPass,
+  render: function (renderer, writeBuffer, readBuffer, deltaTime, maskActive) {
+    if (readBuffer instanceof THREE.WebGLRenderTarget) {
+      this.uniforms["tDiffuse"].value = readBuffer.texture;
+      this.uniforms["tSize"].value.set(readBuffer.width, readBuffer.height);
 
-	render: function ( renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */ ) {
-
-		this.uniforms[ "tDiffuse" ].value = readBuffer.texture;
-		this.uniforms[ "tSize" ].value.set( readBuffer.width, readBuffer.height );
-
-		if ( this.renderToScreen ) {
-
-			renderer.setRenderTarget( null );
-			this.fsQuad.render( renderer );
-
-		} else {
-
-			renderer.setRenderTarget( writeBuffer );
-			if ( this.clear ) renderer.clear();
-			this.fsQuad.render( renderer );
-
-		}
-
-	}
-
-} );
+      if (this.renderToScreen) {
+        renderer.setRenderTarget(null);
+        this.fsQuad.render(renderer);
+      } else {
+        renderer.setRenderTarget(writeBuffer);
+        if (this.clear) renderer.clear();
+        this.fsQuad.render(renderer);
+      }
+    } else {
+      console.error('readBuffer must be a THREE.WebGLRenderTarget');
+    }
+  },
+});
