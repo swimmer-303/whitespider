636a7ef89f130aefcdc3a129d7cd649f04230adf
Spencer Hedges <164781103+swimmer-303@users.noreply.github.com>
2024-05-05 15:52:45 -0400

    improve

--- r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/NormalMapShader.js
+++ r/464a9ca3-8371-41df-a14d-1a89fc9a182e/libs/three/shaders/NormalMapShader.js
@@ -1,6 +1,4 @@
 /**
- * @author alteredq / http://alteredqualia.com/
- *
  * Normal map shader
  * - compute normals from heightmap
  */
@@ -12,7 +10,8 @@ THREE.NormalMapShader = {
 		"heightMap": { value: null },
 		"resolution": { value: new THREE.Vector2( 512, 512 ) },
 		"scale": { value: new THREE.Vector2( 1, 1 ) },
-		"height": { value: 0.05 }
+		"height": { value: new THREE.Vector2( 0.05, 0.05 ) },
+		"bias": { value: new THREE.Vector2( 0, 0 ) }
 
 	},
 
@@ -22,32 +21,4 @@ THREE.NormalMapShader = {
 
 		"void main() {",
 
-		"	vUv = uv;",
-		"	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-
-		"}"
-
-	].join( "\n" ),
-
-	fragmentShader: [
-
-		"uniform float height;",
-		"uniform vec2 resolution;",
-		"uniform sampler2D heightMap;",
-
-		"varying vec2 vUv;",
-
-		"void main() {",
-
-		"	float val = texture2D( heightMap, vUv ).x;",
-
-		"	float valU = texture2D( heightMap, vUv + vec2( 1.0 / resolution.x, 0.0 ) ).x;",
-		"	float valV = texture2D( heightMap, vUv + vec2( 0.0, 1.0 / resolution.y ) ).x;",
-
-		"	gl_FragColor = vec4( ( 0.5 * normalize( vec3( val - valU, val - valV, height  ) ) + 0.5 ), 1.0 );",
-
-		"}"
-
-	].join( "\n" )
-
-};
+	
